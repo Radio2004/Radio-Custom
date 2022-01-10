2d3092120bc26ccca90177a5671cd030319ce9a2
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-10 13:55:19 +0200

    Update c444444445.lua

--- script/c444444445.lua
+++ script/c444444445.lua
@@ -6,14 +6,13 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x1BC}
 function s.cfilter(c)
-	return c:IsSetCard(0x1BC) and c:IsAbleToHandAsCost()and c:IsType(TYPE_MONSTER)
+	return c:IsSetCard(0x1BC) and c:IsAbleToHand()and c:IsType(TYPE_MONSTER)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil) end 
@@ -24,9 +23,9 @@ function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x1BC) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil) end 
+	local g=Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil)
+	Duel.SendtoHand(g,nil,REASON_EFFECT)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
