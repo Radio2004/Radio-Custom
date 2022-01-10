704830bc93f5c626dd9e05f46fff854b734aa3c3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-10 12:25:51 +0200

    a

--- script/c444444442.lua
+++ script/c444444442.lua
@@ -1,4 +1,95 @@
 --Emiliya Yasutoki, Novella Girl
-function c444444442.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	-- effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCountLimit(1,id)   
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+		--Limit battle target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e3:SetValue(s.tg)
+	c:RegisterEffect(e3)
+	--Prevent effect target
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_MZONE,0)
+	e4:SetTarget(s.tg)
+	e4:SetValue(s.tgval)
+	c:RegisterEffect(e4)
 end
+function s.tg(e,c)
+	return c:IsSetCard(0x1BC) and c:GetCode()~=id
+	end
+	function s.tgval(e,re,rp)
+	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP+TYPE_MONSTER) and rp==1-e:GetHandlerPlayer()
+end
+s.listed_series={0x1BC}
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x1BC) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	end
+	function s.thfilter(c)
+	return c:IsSetCard(0x1BC) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+	end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	local g1=Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil)
+	local g2=Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0   
+	local b1=g1
+	local b2=g2
+	if chk==0 then return b1 or b2 end
+	local op=aux.SelectEffect(tp,
+		{b1,aux.Stringid(id,1)},
+		{b2,aux.Stringid(id,0)})
+	e:SetLabel(op)
+	local g=(op==1 and g1 or g2)	
+   Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,k,1,0,0)  
+   Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+	 Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)	 
+		end	 
+	else
+	 local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local c=e:GetHandler()
+		local tc=Duel.GetFirstTarget()
+		if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
+end
+end
+
+	
