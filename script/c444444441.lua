229fb17e8edd19f37c45bc3f944c8334918a5d83
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-10 11:53:47 +0200

    a

---
+++ script/c444444441.lua
@@ -0,0 +1,65 @@
+--Bella von Meller, Novella Girl--
+local s,id=GetID()
+function s.initial_effect(c)
+	--effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x1BC}
+function s.schfilter(c)
+	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		local sel=0
+		if Duel.IsExistingMatchingCard(s.schfilter,tp,LOCATION_DECK,0,1,nil) then sel=sel+2 end
+		e:SetLabel(sel)
+		return sel~=0
+	end
+	local sel=e:GetLabel()
+	if sel==2 then
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+		sel=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))+1
+	elseif sel==1 then
+		Duel.SelectOption(tp,aux.Stringid(id,1))
+	else
+		Duel.SelectOption(tp,aux.Stringid(id,2))
+	end
+	e:SetLabel(sel)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local sel=e:GetLabel()
+	if sel==1 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+		e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+		e1:SetTarget(s.exstg)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.schfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
+function s.exstg(e,c)
+	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER)
+end
