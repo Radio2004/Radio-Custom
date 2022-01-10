991f2353b252bdf0c3ca18fb118ccb911938edce
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-10 13:37:41 +0200

    a

---
+++ script/c444444444.lua
@@ -0,0 +1,68 @@
+--Lumina Feverstein, Novella Girl
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)   
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)  
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x1BC}
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1BC)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+	local g2=Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
+	local b1=g1
+	local b2=g2
+	if chk==0 then return b1 or b2 end
+	local op=aux.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	e:SetLabel(op)
+	local g=(op==1 and g1 or g2)   
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+		local g=Duel.GetMatchingGroup(aux.AND(s.filter,Card.IsCanBeEffectTarget),tp,LOCATION_MZONE,0,nil,e)
+	local att=aux.AnnounceAnotherAttribute(g,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local sel=g:FilterSelect(tp,Card.IsDifferentAttribute,1,1,nil,att)
+	Duel.SetTargetCard(sel)
+	e:SetLabel(att)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(e:GetLabel())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		end
+	else
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)   
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local c=e:GetHandler()
+		local tc=Duel.GetFirstTarget()
+		if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(math.ceil(tc:GetAttack()/2))
+		tc:RegisterEffect(e1)
+	end
+end
+	end
