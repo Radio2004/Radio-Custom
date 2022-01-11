af877baea3eb32ded28b531caf39d6661e0815ef
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-11 22:15:10 +0200

    Update c444444450.lua

--- script/c444444450.lua
+++ script/c444444450.lua
@@ -11,7 +11,7 @@
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetTarget(s.tg)  
 	e1:SetOperation(s.op)
-	c:RegisterEffect(e1)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_16)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
