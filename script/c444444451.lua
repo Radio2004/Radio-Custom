eb19b18d8ccd056e7170658fed8fd1832a29ef10
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-11 19:27:13 +0200

    s

---
+++ script/c444444451.lua
@@ -0,0 +1,133 @@
+--Novella: Happiness
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Change Attribute to EARTH
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e2:SetTarget(s.tg)
+	e2:SetValue(ATTRIBUTE_EARTH)
+	c:RegisterEffect(e2)	
+	--cannot destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+		--def up
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_UPDATE_DEFENSE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTargetRange(LOCATION_MZONE,0)
+	e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1BC))
+	e4:SetValue(s.val)
+	c:RegisterEffect(e4)
+		--effects
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetCategory(CATEGORY_RECOVER+CATEGORY_DESTROY+CATEGORY_DRAW)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_SUMMON_SUCCESS)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,id)
+	e5:SetCondition(s.spcon)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e6:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e6)
+end
+function s.exstg(e,c)
+	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER)
+	end
+function s.val(e,c)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_MZONE)*200
+end
+function s.tg(e,c,tp)
+	if not c:IsSetCard(0x1BC) then return false end
+	if c:GetFlagEffect(1)==0 then
+		c:RegisterFlagEffect(1,0,0,0)
+		local eff
+		if c:IsLocation(LOCATION_MZONE) then
+			eff={Duel.GetPlayerEffect(c:GetControler(),EFFECT_NECRO_VALLEY)}
+		else
+			eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+		end
+		c:ResetFlagEffect(1)
+		for _,te in ipairs(eff) do
+			local op=te:GetOperation()
+			if not op or op(e,c) then return false end
+		end
+	end
+	return true
+end
+function s.filter1(c)
+	return c:IsFaceup()and c:IsSetCard(0x1BC)
+	end
+	function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter1,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	end
+	function s.operation(e,tp,eg,ep,ev,re,r,rp)
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetTarget(s.exstg)
+		e1:SetValue(1)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		end
+	function s.desfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1BC)
+	end
+	function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x1BC),tp,LOCATION_MZONE,0,1,nil)
+	local g2=Duel.IsExistingTarget(s.desfilter,tp,LOCATION_MZONE,0,1,nil)and Duel.IsPlayerCanDraw(tp,2)
+	local b1=g1
+	local b2=g2
+	if chk==0 then return b1 or b2 end
+	local op=aux.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	e:SetLabel(op)
+	local g=(op==1 and g1 or g2)
+		local rec=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsSetCard,0x1BC),tp,LOCATION_MZONE,0,nil)*300
+	Duel.SetTargetParam(rec)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,rec)
+	Duel.SetTargetPlayer(tp)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+		local rec=Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsSetCard,0x1BC),tp,LOCATION_MZONE,0,nil)*300
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Recover(p,rec,REASON_EFFECT)
+	else
+		local k=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,k,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+		local tc=Duel.GetFirstTarget()
+		if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+			Duel.BreakEffect()
+			Duel.Destroy(tc,REASON_EFFECT)
+			Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end
+end
