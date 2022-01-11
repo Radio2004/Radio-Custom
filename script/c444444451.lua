a35c551b0fcdf5e7a24bc7604d8310981e5d7d8c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-11 19:30:29 +0200

    Update c444444451.lua

--- script/c444444451.lua
+++ script/c444444451.lua
@@ -4,6 +4,7 @@ function s.initial_effect(c)
 	--activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCountLimit(1,{id,1})
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
 	--Change Attribute to EARTH
