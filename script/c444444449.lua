ecd3776543d1744911167b50247ff3425f88e9c4
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-12 21:22:45 +0200

    Update c444444449.lua

--- script/c444444449.lua
+++ script/c444444449.lua
@@ -89,7 +89,7 @@ function s.ssop(c)
 		if #acteffs>0 then
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EFFECT)
 			local i=Duel.SelectOption(tp,table.unpack(desc))
-			local eff=acteffs(i+1)
+			local eff=i+1
 			if cost then cost(e,tp,eg,ep,ev,re,r,rp,1) end
 			if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
 			local op=eff:GetOperation()
