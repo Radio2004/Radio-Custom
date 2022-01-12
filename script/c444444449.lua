5ae70abbdbb60379d73d2b62ffcec6f2bb42e15a
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-12 21:23:39 +0200

    Update c444444449.lua

--- script/c444444449.lua
+++ script/c444444449.lua
@@ -92,7 +92,7 @@ function s.ssop(c)
 			local eff=i+1
 			if cost then cost(e,tp,eg,ep,ev,re,r,rp,1) end
 			if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
-			local op=eff:GetOperation()
+			local op=acteffs:GetOperation()
 			op(e,tp,eg,ep,ev,re,r,rp)
 			table.remove(acteffs,eff)
 			table.remove(desc,eff)
