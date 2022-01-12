bebed93a8287f1d03816099c46e9269200c83286
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-12 22:14:34 +0200

    Update c444444449.lua

--- script/c444444449.lua
+++ script/c444444449.lua
@@ -93,7 +93,7 @@ function s.ssop(c)
 			if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
 			local op=eff:GetOperation()
 			op(e,tp,eg,ep,ev,re,r,rp)
-			if op then op(e,tp,eg,ep,ev,re,r,rp,1) end
+			
 		end
 	end
 end
