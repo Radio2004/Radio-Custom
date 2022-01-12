65fb49fad3405421211dfba967345dfe68c5d509
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-12 21:48:55 +0200

    Update c444444449.lua

--- script/c444444449.lua
+++ script/c444444449.lua
@@ -95,7 +95,7 @@ function s.ssop(c)
 			local op=eff:GetOperation()
 			op(e,tp,eg,ep,ev,re,r,rp)
 			table.remove(acteffs,1)
-			table.remove(desc,desc[i+1])
+			table.remove(desc,1)
 		end
 	end
 end
