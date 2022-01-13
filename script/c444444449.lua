93bc77f3e874b44d17746fcd8e5382ea646d39ea
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-13 14:42:49 +0200

    Update c444444449.lua

--- script/c444444449.lua
+++ script/c444444449.lua
@@ -83,8 +83,8 @@ function s.ssop(c)
 		for _,eff in ipairs(effs) do
 			con,tg=eff:GetCondition(),eff:GetTarget()
 			if (not con or con(e,tp,eg,ep,ev,re,r,rp)) and (not cost or cost(e,tp,eg,ep,ev,re,r,rp,0)) and (not tg or tg(e,tp,eg,ep,ev,re,r,rp,0)) then
-				table.insert(acteffs,eff:GetLabelObject():GetDescription())
-				table.insert(desc,eff:GetLabelObject():GetDescription())
+				table.insert(acteffs,eff:GetLabelObject())
+				table.insert(desc,eff:GetLabelObject():GetDescription():GetDescription(1))
 			end
 		end
 		while #acteffs>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) do
