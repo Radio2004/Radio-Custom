a1da000b1868ca4e199b93b7775fa8385cce26a3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-01-19 08:41:06 +0200

    ы

---
+++ script/c444444456.lua
@@ -0,0 +1,57 @@
+--Novella's Book
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1BC))
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+		--avoid battle damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetCondition(s.condition)
+	c:RegisterEffect(e3)
+	end
+	function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1BC) and c:IsLevelAbove(7) and c:IsType(TYPE_MONSTER)or c:IsType(TYPE_LINK)
+	end
+	function s.condition(e)
+	return Duel.IsExistingMatchingCard(s.filter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
+end
+function s.filter1(c,tp)
+	return c:IsSetCard(0x1BC) and not c:IsPublic()
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,c:GetLevel())
+end
+function s.filter2(c,lv)
+	return  c:IsSetCard(0x1BC)  and c:IsType(TYPE_MONSTER) and c:GetLevel()<lv and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end 
+	local g=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_HAND,0,nil,tp)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0))then
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local tc=g:Select(tp,1,1,nil)
+	Duel.ConfirmCards(1-tp,tc)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g2=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil,tc:GetFirst():GetLevel())
+	Duel.SendtoHand(g2,nil,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g2)
+	end
+end
+	
+
