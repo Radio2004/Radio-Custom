e62aa0ca12124f35160c27cfb79b7ace15246866
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-02-12 15:40:18 +0200

    a

---
+++ script/c202200001.lua
@@ -0,0 +1,30 @@
+--The Wonder of World Mouse Archer
+local s,id=GetID()
+function s.initial_effect(c)
+	--spirit return
+	aux.EnableSpiritReturn(c,EVENT_SUMMON_SUCCESS,EVENT_FLIP)
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.FALSE)
+	c:RegisterEffect(e1)
+	--Reveal
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.target)
+	
+	c:RegisterEffect(e2)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 and e:GetHandler():GetFlagEffect(id)==0
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsPublic,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.SetTargetPlayer(tp)
+end
+
+
+
