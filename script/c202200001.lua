61ca05365b81ba5625c9ebbca8400654a122713e
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-02-13 17:25:43 +0200

    Update c202200001.lua

--- script/c202200001.lua
+++ script/c202200001.lua
@@ -38,6 +38,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	   local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil)
 	   if #g>=2 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 	   local f=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_HAND,0,nil)
+	Duel.SetTargetPlayer(1-tp)
+
 	   Duel.Damage(p,f*200,REASON_EFFECT)
 end
 end
