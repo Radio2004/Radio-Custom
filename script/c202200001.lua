f9c7da2bed8bbb5b168cce6c3785611099d7fa0c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-02-13 10:27:04 +0200

    Update c202200001.lua

--- script/c202200001.lua
+++ script/c202200001.lua
@@ -25,19 +25,24 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 and e:GetHandler():GetFlagEffect(id)==0
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsPublic,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,e:GetHandler())
+		and not Duel.IsExistingMatchingCard(Card.IsPublic,tp,LOCATION_HAND,0,1,e:GetHandler()) end
 	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
-	local ct=Duel.GetFieldGroup(p,LOCATION_HAND,0)
-	if ct==1 then return end
-		Duel.BreakEffect()
-	   local g=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,0,nil)
-	   if #g>0 and ct>=2 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-	   Duel.Damage(p,g*200,REASON_EFFECT)
+	local cg=Duel.GetFieldGroup(p,LOCATION_HAND,0)
+	Duel.ConfirmCards(1-p,cg)
+	local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,p,LOCATION_HAND,0,nil)
+	if #g>=1 then
+		Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+		local sg=g:Select(p,1,1,nil)
+		Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+	end
+	Duel.ShuffleHand(p)
 end
-end
+
 
 
  
