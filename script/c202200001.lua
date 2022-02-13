c620d6f9a2191d2f958f3b0d8e214a6664ea9fd3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-02-13 17:01:47 +0200

    Update c202200001.lua

--- script/c202200001.lua
+++ script/c202200001.lua
@@ -44,7 +44,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if ct==1 then return end
 		Duel.BreakEffect()
 	   local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_HAND,0,nil)
-	   if #g>=0 and ct==2 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+	   if #g>=2 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
 	   Duel.Damage(p,g*200,REASON_EFFECT)
 end
 end
