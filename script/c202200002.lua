ad0883892836bdce5af1bef9664e059062200980
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-02-13 21:56:29 +0200

    Update c202200002.lua

--- script/c202200002.lua
+++ script/c202200002.lua
@@ -9,6 +9,18 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--Destroy
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCode(EVENT_RELEASE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
 end
 function s.thfilter(c)
 	return c:IsType(TYPE_SPIRIT) and c:IsLevelAbove(6) and c:IsAbleToHand()
@@ -23,5 +35,26 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,sg)
 	end
 end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+   local re=c:GetReasonEffect()
+	if not re then return false end
+	local rc=re:GetHandler()
+	return c:IsType(TYPE_SPIRIT) and c:IsPreviousControler(tp)
+		and c:IsReason(REASON_RETURN) and rc==c
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return true end 
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+ if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+
 
 
