052f2e9d567215239189ee723d908c53ec5b1aae
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-02-19 17:11:31 +0200

    s

--- script/c202200002.lua
+++ script/c202200002.lua
@@ -35,9 +35,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return c:IsType(TYPE_SPIRIT) and c:IsPreviousControler(tp)
-		and aux.EnableSpiritReturn(c,EVENT_SUMMON_SUCCESS,EVENT_FLIP)
+	local tc=eg:GetFirst()
+	return #eg==1 and tc:IsControler(tp) and tc:IsPreviousControler(tp) 
+		and tc:GetReasonEffect() and tc:GetReasonEffect():GetOwner()==tc
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return true end
@@ -45,7 +45,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
  if not e:GetHandler():IsRelateToEffect(e) then return end
 	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
-	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+	if #g>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
