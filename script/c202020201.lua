d857565038b9752c5f3a16cd80d79bc719164a62
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-02-24 15:32:21 +0200

    і

---
+++ script/c202020201.lua
@@ -0,0 +1,4 @@
+--Samurai Slayer - Jirokichi
+function c202020201.initial_effect(c)
+	
+end
--- script/c202200003.lua
+++ script/c202200003.lua
@@ -1,4 +1,32 @@
 --The Wonder of World Robot Kunoichi
-function c202200003.initial_effect(c)
-	
-end
+local s,id=GetID()
+function s.initial_effect(c)
+	--spirit return
+	aux.EnableSpiritReturn(c,EVENT_SUMMON_SUCCESS,EVENT_FLIP,EVENT_SPSUMMON_SUCCESS)
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.FALSE)
+	c:RegisterEffect(e1)
+	--Reveal
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCondition(s.spcon)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.activate)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	c:RegisterEffect(e4)
+	end
+
+
+
