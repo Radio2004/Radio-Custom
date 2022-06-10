f63fc9a538667f8cf0822a3f83b83b4ea4500c38
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-10 09:52:11 +0300

    Update c890900002.lua

--- script/c890900002.lua
+++ script/c890900002.lua
@@ -72,19 +72,24 @@ end
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_ATTACK_COST)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e1:SetTargetRange(0,1)
-		e1:SetOperation(s.atop)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_ATTACK_COST)
+	e2:SetCost(s.atcost)
+	e2:SetOperation(s.atop)
+
 		tc:RegisterEffect(e1)
 	end
 end
-	function s.atop(e,tp,eg,ep,ev,re,r,rp)
-		Duel.PayLPCost(tp,500)
-		Duel.AttackCostPaid()
+	function s.atcost(e,c,tp)
+	return Duel.CheckLPCost(tp,1000)
 end
+	function s.atop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsAttackCostPaid()~=2 and e:GetHandler():IsLocation(LOCATION_MZONE) then
+		Duel.PayLPCost(tp,1000)
+		Duel.AttackCostPaid()
+	end
+end
+
 
 
 
