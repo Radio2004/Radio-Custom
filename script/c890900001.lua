8cfdb37f15687722a4ecca1faa714c7ea162c1da
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-10 07:37:50 +0300

    Update c890900001.lua

--- script/c890900001.lua
+++ script/c890900001.lua
@@ -26,7 +26,7 @@
 	c:RegisterEffect(e2)
 end
 	function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetPreviousLocation()==LOCATION_HAND and (r&REASON_DISCARD)~=0
+	return e:GetHandler():GetPreviousLocation()==LOCATION_HAND and (r&REASON_DISCARD)~=0 and Duel.GetTurnPlayer()==tp
 end
 	function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
