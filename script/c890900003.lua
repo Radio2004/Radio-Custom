7512ba40e426bcc1275d77d5591987150f9ff11f
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-12 19:02:27 +0300

    Update c890900003.lua

--- script/c890900003.lua
+++ script/c890900003.lua
@@ -74,7 +74,7 @@ end
 		Duel.ConfirmDecktop(tp,1)
 		local g=Duel.GetDecktopGroup(tp,1)
 		local tg=g:GetFirst()
-		if tg:IsType(TYPE_MONSTER) and tg:IsAbleToHand() then
+		if tg:IsType(TYPE_MONSTER) and tg:IsSetCard(0x3dd) and tg:IsAbleToHand() then
 		Duel.DisableShuffleCheck()
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ShuffleHand(tp)
