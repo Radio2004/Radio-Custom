472a0757e1a91c7364778263aa0004277887296d
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-12 18:38:36 +0300

    Update c890900003.lua

--- script/c890900003.lua
+++ script/c890900003.lua
@@ -62,21 +62,21 @@ end
 end
 	function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsFaceup() and chkc:IsLocation(LOCATION_MZONE) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
 end
 	function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_ATTACK_COST)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetCost(s.atcost)
-		e1:SetOperation(s.atop)
-		tc:RegisterEffect(e1)
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then
+		Duel.ConfirmDecktop(tp,1)
+		local g=Duel.GetDecktopGroup(tp,1)
+		local tc=g:GetFirst()
+		if tc:IsType(TYPE_SPELL) and tc:IsAbleToHand() then
+		Duel.DisableShuffleCheck()
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ShuffleHand(tp)
 		--Destroy
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
@@ -84,5 +84,11 @@ end
 		e2:SetOperation(s.desop)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
+	end
+end
+	function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsAttackPos() or c:IsDefensePos() then
+		Duel.Destroy(c,REASON_EFFECT)
 	end
 end
