3b625af67a4117cb2588c26325deac577741d715
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-12 17:31:25 +0300

    Update c890900002.lua

--- script/c890900002.lua
+++ script/c890900002.lua
@@ -83,7 +83,6 @@ end
 		local e2=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
 	e2:SetCode(EFFECT_SELF_DESTROY)
 	e2:SetCondition(s.sdcon)
 
