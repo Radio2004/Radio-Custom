7c65e9ed7e6c4461649cfdad17bbe4a469b3f8fa
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-13 20:15:17 +0300

    Update c890900003.lua

--- script/c890900003.lua
+++ script/c890900003.lua
@@ -82,6 +82,7 @@ end
 		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e3)
 	if tg:IsType(TYPE_MONSTER) and tg:IsSetCard(0x3dd) and (not tg:IsAbleToGrave() or Duel.SelectYesNo(tp,aux.Stringid(id,1))) then
+			Duel.DisableShuffleCheck()
 			local atk=tg:GetAttack()
 			local def=tg:GetDefense()
 			local e1=Effect.CreateEffect(c)
@@ -94,6 +95,7 @@ end
 			e2:SetCode(EFFECT_UPDATE_DEFENSE)
 			e2:SetValue(-def)
 			tc:RegisterEffect(e2)
+			Duel.ShuffleDeck(tp)
 		else
 			Duel.DisableShuffleCheck()
 			Duel.SendtoGrave(tg,REASON_EFFECT+REASON_REVEAL)
