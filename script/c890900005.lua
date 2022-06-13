d93a2ac1de7273a71d260c48a95f59db59f44edc
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-13 20:44:05 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -78,7 +78,7 @@ end
 		e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		e1:SetCountLimit(1)
-		e1:SetCost(s.costchk)
+		e1:SetOperation(s.costchk)
 		e1:SetValue(s.valcon)
 		tc:RegisterEffect(e1)
 		--Destroy
