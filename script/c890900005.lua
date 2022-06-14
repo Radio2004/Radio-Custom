992e83433c4cfb0abce3e12152e6e83279a44c1f
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-14 16:09:36 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -95,9 +95,10 @@ end
 		Duel.Destroy(c,REASON_EFFECT)
 	end
 end
-	function s.valcon(tp,e,re,r,rp)
+	function s.valcon(e,re,r,rp)
 	local c=e:GetHandler()
 	if r & REASON_BATTLE ==0 then return 0 end
+	local tp=e:GetHandlerPlayer()
 	local dam=Duel.GetLP(tp,c:GetAttack(),REASON_EFFECT)
 	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
 	local e1=Effect.CreateEffect(c)
