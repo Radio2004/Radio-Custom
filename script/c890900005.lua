c5cea7a66194b1e8c3289aff82f8356c60c13444
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-14 10:58:45 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -114,7 +114,8 @@ function s.repval(e,c)
 	return s.repfilter(c,e:GetHandlerPlayer())
 end
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+	local c=e:GetHandler()
+	Duel.Recover(tp,c:GetAttack(),REASON_EFFECT)
 end
 
 
