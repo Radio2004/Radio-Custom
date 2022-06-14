1fd328fc507ff0e4b67e0fbd031e9a010762d531
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-14 15:59:27 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -107,7 +107,7 @@ end
 	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCode(EVENT_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(dam)
