6fa24fadabf8d5180e9856be9599a654067fc6d3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-14 16:05:31 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -98,7 +98,7 @@ end
 	function s.valcon(e,re,r,rp)
 	local c=e:GetHandler()
 	if r & REASON_BATTLE ==0 then return 0 end
-	local dam=Duel.GetLP(tp,tc:GetAttack(),REASON_EFFECT)
+	local dam=Duel.GetLP(tp,c:GetAttack(),REASON_EFFECT)
 	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
