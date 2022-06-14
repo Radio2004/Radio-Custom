6a4de13bbffc040219167cdb9e4b2dc19f640e58
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-14 16:03:32 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -102,7 +102,7 @@ end
 	local a=Duel.GetAttacker()
 	local tc=a:GetBattleTarget()
 	if tc and tc:IsControler(1-tp) then a,tc=tc,a end
-	local dam=Duel.GetBattleDamage(tp)
+	local dam=Duel.GetLP(tp)
 	if not tc or dam<=0 then return 1 end
 	c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
 	local e1=Effect.CreateEffect(c)
