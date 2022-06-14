8504bb0b53500f04b9ed901544919171e86f2193
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-14 10:11:40 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -73,6 +73,7 @@ end
 		--battle indes
 	local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 		e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		e1:SetCountLimit(1)
