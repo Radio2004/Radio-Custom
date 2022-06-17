1ade9c9bee565749a30620b0598c96b393ea6421
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-17 19:09:32 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -103,7 +103,7 @@ end
 	if r & REASON_BATTLE ==0 then return 0 end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetCode(EFFECT_REVERSE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(dam)
