d43f83f3a9264970f8ff6dc7d82f013e0a8ed847
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-17 10:08:30 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -88,7 +88,7 @@ end
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		e1:SetValue(s.valcon)
 		tc:RegisterEffect(e1)
-		if tc:IsRelateToEffect(e1) then
+		if not tc:IsRelateToEffect(e1)  then
 			Duel.Damage(1-tp,dam,REASON_EFFECT)
 	end
 end
