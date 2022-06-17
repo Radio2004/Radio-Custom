6c8c7e9d8ab5041d2580e00e8e5538c245e98a83
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-17 19:45:38 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -99,14 +99,9 @@ end
  function s.valcon(e,re,r,rp)
 	local c=e:GetHandler()
 	local tp=e:GetHandlerPlayer()
-	local dam=c:GetAttack()
+	local dam=c:Duel.Damage()
 	if r & REASON_BATTLE ==0 then return 0 end
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(dam)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	c:RegisterEffect(e1)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
 	return 1
 end
