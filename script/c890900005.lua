e95c05f1930c1941d9b08e442ca68146be9d011f
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-17 19:41:45 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -103,10 +103,9 @@ end
 	if r & REASON_BATTLE ==0 then return 0 end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_NO_BATTLE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(1)
+	e1:SetValue(dam)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	c:RegisterEffect(e1)
 	return 1
