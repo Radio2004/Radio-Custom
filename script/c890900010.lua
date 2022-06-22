4b3e9c73c106f59682a8f2f31bb0a5afd3db0042
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-06-22 19:02:21 +0300

    Update c890900010.lua

--- script/c890900010.lua
+++ script/c890900010.lua
@@ -64,12 +64,14 @@ end
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsFaceup() and chkc:IsLocation(LOCATION_MZONE) end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SelectTarget(tp,Card.IsCanAddCounter,tp,0,LOCATION_MZONE,1,1,nil,0x1015,1)
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0)
 end
 	function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		tc:AddCounter(0x1015,1)
 		--Destroy
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
@@ -77,14 +79,14 @@ end
 		e1:SetOperation(s.desop)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
-		--Cannot Attack
+		--gain ATK
 		local e2=Effect.CreateEffect(c)
+		e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_CANNOT_ATTACK)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e2:SetCondition(s.rcon)
-		tc:RegisterEffect(e2)
-
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetValue(-s.atkval)
+		c:RegisterEffect(e2)
 	end
 end
    function s.desop(e,tp,eg,ep,ev,re,r,rp)
@@ -93,6 +95,6 @@ end
 		Duel.Destroy(c,REASON_EFFECT)
 	end
 end
-	function s.rcon(e)
-	return e:GetOwner():IsHasCardTarget(e:GetHandler())
+	function s.atkval(e,c)
+	return Duel.GetCounter(0,1,1,0x1015)*200
 end
