d5b09c72d7455355b115e2020a472e637f71d742
Radio2004 <Andrijsupa@gmail.com>
2022-06-26 17:19:06 +0300

    Update c890900013.lua

--- script/c890900013.lua
+++ script/c890900013.lua
@@ -4,6 +4,14 @@
 	--xyz summon
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3dd),5,3)
 	c:EnableReviveLimit()
+	--Add WIND attribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(ATTRIBUTE_WATER)
+	c:RegisterEffect(e1)
 	--"Melirria" can choose to not return
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
