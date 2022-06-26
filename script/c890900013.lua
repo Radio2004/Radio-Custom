5448cd22d03948186c57098663afb83174e7e4b7
Radio2004 <Andrijsupa@gmail.com>
2022-06-26 19:51:36 +0300

    Update c890900013.lua

--- script/c890900013.lua
+++ script/c890900013.lua
@@ -70,10 +70,9 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) then
-		Duel.Damage(1-tp,500,REASON_EFFECT)
+		Duel.Damage(1-tp,ev:GetLevel()*200,REASON_EFFECT)
 	end
 end
