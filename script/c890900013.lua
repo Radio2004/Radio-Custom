724139e2680976770f7c9ee4a740680902c939f1
Radio2004 <Andrijsupa@gmail.com>
2022-06-26 16:52:03 +0300

    a

--- script/c890900013.lua
+++ script/c890900013.lua
@@ -1,16 +1,14 @@
---Melirria space
+--Melirria, Episode of Fire and Ice - The Great Witch
 	local s,id=GetID()
-function s.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
---"Melirria" Fusions can choose to not return
+	function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3dd),5,3)
+	c:EnableReviveLimit()
+	--"Melirria" can choose to not return
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(id)
-	e3:SetRange(LOCATION_FZONE)
-	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
 	c:RegisterEffect(e3)
 end
---
+++ script/c890900113.lua
@@ -0,0 +1,16 @@
+--Melirria space
+	local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+--"Melirria" Fusions can choose to not return
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(id)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	c:RegisterEffect(e3)
+end
