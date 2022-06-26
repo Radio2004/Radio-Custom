01b85223fc599e4336dca35c419aeaa7ede0ffe0
Radio2004 <Andrijsupa@gmail.com>
2022-06-26 16:17:24 +0300

    Update c890900013.lua

--- script/c890900013.lua
+++ script/c890900013.lua
@@ -1,4 +1,11 @@
 --Melirria space
+	local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
 --"Melirria" Fusions can choose to not return
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
@@ -6,3 +13,4 @@
 	e3:SetRange(LOCATION_FZONE)
 	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
 	c:RegisterEffect(e3)
+end
