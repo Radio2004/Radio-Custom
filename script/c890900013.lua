1511c30e4981405f9953ea3af94727bd2ffe2614
Radio2004 <Andrijsupa@gmail.com>
2022-06-26 17:51:44 +0300

    Update c890900013.lua

--- script/c890900013.lua
+++ script/c890900013.lua
@@ -12,6 +12,14 @@
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetValue(ATTRIBUTE_WATER)
 	c:RegisterEffect(e1)
+	--direct attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_DIRECT_ATTACK)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.attg)
+	c:RegisterEffect(e2)
 	--"Melirria" can choose to not return
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
@@ -19,4 +27,7 @@
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetTargetRange(LOCATION_MZONE,0)
 	c:RegisterEffect(e3)
+end
+	function s.attg(e,c)
+	return c:IsSetCard(0x1fa3) and c:IsFaceup() and not c:IsCode(id)
 end
