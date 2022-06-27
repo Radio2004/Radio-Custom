d1fcace63b83d8a3249df18315947b874483f69f
Radio2004 <Andrijsupa@gmail.com>
2022-06-27 11:22:48 +0300

    Update c890900015.lua

--- script/c890900015.lua
+++ script/c890900015.lua
@@ -6,13 +6,19 @@
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
+	local e2=e3:Clone()
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	c:RegisterEffect(e2)
 	--indes
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e3:SetRange(LOCATION_FZONE)
 	e3:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
-	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x3dd))
+	e3:SetTarget(s.tg)
 	e3:SetValue(1)
 	c:RegisterEffect(e3)
 end
+	function s.tg(e,c)
+	return c:IsFaceup() and c:IsSetCard(0x3dd) and c:GetCode()~=id
+	end
