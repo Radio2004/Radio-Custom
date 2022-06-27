f7109bb36c09bc33b45872b9e68955b85d2fc9da
Radio2004 <Andrijsupa@gmail.com>
2022-06-27 11:40:36 +0300

    a

--- script/c890900015.lua
+++ script/c890900015.lua
@@ -18,7 +18,39 @@
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	c:RegisterEffect(e3)
+	--search
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e4:SetCost(s.thcost)
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
 end
 	function s.tg(e,c)
 	return c:IsFaceup() and c:IsSetCard(0x3dd) and c:GetCode()~=id
 	end
+	function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsSetCard,0x3dd,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	Duel.DiscardHand(tp,Card.IsSetCard,0x3dd,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.filter1(c)
+	return c:IsSetCard(0x3dd) and c:IsAbleToHand()
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation1(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
