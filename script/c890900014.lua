27ee2a95e16a7e466b33a29fff3aba12fa89bf48
Radio2004 <Andrijsupa@gmail.com>
2022-06-27 10:16:23 +0300

    Update c890900014.lua

--- script/c890900014.lua
+++ script/c890900014.lua
@@ -74,8 +74,8 @@ end
 	function s.filter1(c)
 	return c:IsFaceup() and c:IsSetCard(0x3dd) and c:IsAttribute(ATTRIBUTE_DARK)
 end
-	function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_MZONE,0,1,nil)
+	function s.tgcon(e)
+	return Duel.IsEnvironment(0x3dd)
 end
 	function s.psplimit(e,c,tp,sumtp,sumpos)
 	return not c:IsSetCard(0x3dd) and (sumtp&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM 
