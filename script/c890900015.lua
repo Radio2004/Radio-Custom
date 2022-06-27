c509575212ef3247e2a19f70d4ede7d7525db8b6
Radio2004 <Andrijsupa@gmail.com>
2022-06-27 11:57:43 +0300

    a

--- script/c890900015.lua
+++ script/c890900015.lua
@@ -29,6 +29,21 @@
 	e4:SetTarget(s.target1)
 	e4:SetOperation(s.operation1)
 	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetTarget(s.target2)
+	e5:SetOperation(s.operation2)
+	c:RegisterEffect(e5)
+	local e6=e4:Clone()
+	e6:SetDescription(aux.Stringid(id,2)
+	e6:SetTarget(s.target3
+	e6:SetOperation(s.operation3)
+	c:RegisterEffect(e6)
+	local e7=e4:Clone()
+	e7:SetDescription(aux.Stringid(id,3)
+	e7:SetTarget(s.target4)
+	e7:SetOperation(s.operation4)
+	c:RegisterEffect(e7)
 end
 	function s.tg(e,c)
 	return c:IsFaceup() and c:IsSetCard(0x3dd) and c:GetCode()~=id
@@ -41,7 +56,7 @@ end
 	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD)
 end
 function s.filter1(c)
-	return c:IsSetCard(0x3dd) and c:IsAbleToHand()
+	return c:IsSetCard(0x38d) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
 function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil) end
@@ -51,6 +66,54 @@ function s.operation1(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+	function s.filter2(c)
+	return c:IsSetCard(0x2704) and c:IsType(TYPE_EQUIP) and c:IsAbleToHand()
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+	function s.filter3(c)
+	return c:IsSetCard(0x22c3) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter3,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
+end
+function s.operation3(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter3,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+	function s.filter4(c)
+	return c:IsSetCard(0x1fa3) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.target4(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter4,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation4(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter4,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
