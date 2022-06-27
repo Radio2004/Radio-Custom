0eac00d03617143124946c64ffd848cd5100ec1d
Radio2004 <Andrijsupa@gmail.com>
2022-06-27 11:13:03 +0300

    a

---
+++ script/c890900015.lua
@@ -0,0 +1,18 @@
+--Melirria
+	local s,id=GetID()
+	function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--indes
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x3dd))
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
