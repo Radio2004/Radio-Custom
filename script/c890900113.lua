58ef6ed209a04943a523ad19f594b8844f9f9323
Radio2004 <Andrijsupa@gmail.com>
2022-06-29 14:29:18 +0300

    a

---
+++ script/c890900113.lua
@@ -0,0 +1,19 @@
+--Melirria, Episode of Fire and Ice - The Great Witch
+	local s,id=GetID()
+	function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3dd),5,5)
+	c:EnableReviveLimit()
+	--"Melirria" can choose to not return
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(id)
+	e3:SetRange(LOCATION_MZONE)
+	e3:operation(s.asd)
+	c:RegisterEffect(e3)
+end
+	function s.condition(e,c)
+	return c:SetTargetParam(GetOverlayCount())
+end
+	
+	
