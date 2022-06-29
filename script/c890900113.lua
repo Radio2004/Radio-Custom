3fac3af7f7259c78de6aab12b2f7dcf5ca0e5d3a
Radio2004 <Andrijsupa@gmail.com>
2022-06-29 20:48:27 +0300

    Update c890900113.lua

--- script/c890900113.lua
+++ script/c890900113.lua
@@ -3,13 +3,13 @@
 	local s,id=GetID()
 	function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3dd),5,5)
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3dd),5,3)
 	c:EnableReviveLimit()
 	--double xyz material
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(id)
-	e2:SetValue(2)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_SINGLE)
