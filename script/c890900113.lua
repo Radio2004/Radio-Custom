ff2a18c4e6f1580a96e9675233cb1e572c7af647
Radio2004 <Andrijsupa@gmail.com>
2022-06-29 23:13:31 +0300

    Update c890900113.lua

--- script/c890900113.lua
+++ script/c890900113.lua
@@ -16,7 +16,7 @@
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetDescription(aux.Stringid(id,0))
 		ge1:SetType(EFFECT_TYPE_FIELD)
-		ge1:SetCode(EFFECT_SUMMON_PROC)
+		ge1:SetCode(EFFECT_SPSUMMON_PROC)
 		ge1:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
 		ge1:SetCondition(s.sumcon)
 		ge1:SetTarget(aux.FieldSummonProcTg(aux.TargetBoolFunction(aux.TRUE),s.sumtg))
@@ -33,12 +33,12 @@ end
 	local mi,ma=c:GetTributeRequirement()
 	if mi<minc then mi=minc end
 	if ma<mi then return false end
-	return ma>0 and Duel.IsExistingMatchingCard(s.castlefilter,c:GetControler(),LOCATION_MZONE,0,1,nil,c:GetControler(),mi,ma)
+	return ma>0 and Duel.CheckReleaseGroup(s.castlefilter,c:GetControler(),LOCATION_MZONE,0,1,nil,c:GetControler(),mi,ma)
 end
 function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk,c)
 	tp=c:GetControler()
 	local mi,ma=c:GetTributeRequirement()
-	local sg=Duel.SelectMatchingCard(tp,s.castlefilter,tp,LOCATION_MZONE,0,1,1,true,nil,tp,mi,ma)
+	local sg=Duel.SelectReleaseGroup(tp,s.castlefilter,tp,LOCATION_MZONE,0,1,1,true,nil,tp,mi,ma)
 	if sg then
 		sg:KeepAlive()
 		e:SetLabelObject(sg)
