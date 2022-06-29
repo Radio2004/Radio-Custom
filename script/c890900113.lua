c8ae668682d385dbb95a8782a7aec2598747abc9
Radio2004 <Andrijsupa@gmail.com>
2022-06-29 21:49:32 +0300

    Update c890900113.lua

--- script/c890900113.lua
+++ script/c890900113.lua
@@ -21,7 +21,7 @@
 		ge1:SetCondition(s.sumcon)
 		ge1:SetTarget(aux.FieldSummonProcTg(aux.TargetBoolFunction(Card.IsSetCard,0x3de),s.sumtg))
 		ge1:SetOperation(s.sumop)
-		ge1:SetValue(SUMMON_TYPE_TRIBUTE)
+		ge1:SetValue(SUMMON_TYPE_SPECIAL)
 		Duel.RegisterEffect(ge1,0)
 	end)
 end
