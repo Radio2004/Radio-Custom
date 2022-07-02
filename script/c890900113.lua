60a45b46c5c59738bcc1ba4cb0d88e56a7d31d46
Radio2004 <Andrijsupa@gmail.com>
2022-07-02 20:27:24 +0300

    Update c890900113.lua

--- script/c890900113.lua
+++ script/c890900113.lua
@@ -14,39 +14,50 @@
 	aux.GlobalCheck(s,function()
 		--summon proc
 		local ge1=Effect.CreateEffect(c)
-		ge1:SetDescription(aux.Stringid(id,0))
-		ge1:SetType(EFFECT_TYPE_FIELD)
-		ge1:SetCode(EFFECT_SPSUMMON_PROC)
-		ge1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-		ge1:SetTargetRange(LOCATION_HAND,0)
-		ge1:SetCondition(s.sumcon)
-		ge1:SetTarget(aux.TargetBoolFunction(Card.IsCode,890900018),s.sumtg)
-		ge1:SetOperation(s.sumop)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		ge1:SetOperation(s.op)
 		Duel.RegisterEffect(ge1,0)
 	end)
 end
-function s.rfilter(c,tp)
-	return c:IsSetCard(0x3dd) and (c:IsControler(tp) or c:IsFaceup())
+	function s.filter(c)
+	return c:IsCode(890900018) 
 end
-function s.sumcon(e,c,minc)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local rg=Duel.GetReleaseGroup(tp):Filter(s.rfilter,nil,tp)
-	return aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0)
-end
-function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk,c)
-	local rg=Duel.GetReleaseGroup(tp):Filter(s.rfilter,nil,tp)
-	local mg=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_RELEASE,nil,nil,true)
-	if #mg==1 then
-		mg:KeepAlive()
-		e:SetLabelObject(mg)
-		return true
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(s.filter,0,LOCATION_HAND,LOCATION_HAND,nil)
+	local tc=g1:GetFirst()
+	while tc do
+			local e2=Effect.CreateEffect(tc)
+			e2:SetDescription(aux.Stringid(64382841,3))
+			e2:SetType(EFFECT_TYPE_FIELD)
+			e2:SetCode(EFFECT_SPSUMMON_PROC)
+			e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+			e2:SetRange(LOCATION_HAND)
+			e2:SetCondition(s.plasmacon)
+			e2:SetOperation(s.plasmaop)
+			end
+			tc:RegisterEffect(e2)
+		end
+		tc=g1:GetNext()
 	end
-	return false
 end
-function s.sumop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=e:GetLabelObject()
-	if not g then return end
-	Duel.Release(g,REASON_COST)
-	g:DeleteGroup()
+function s.plasmacon(e,c)
+	if c==nil then return true end
+	local g=Duel.GetReleaseGroup(c:GetControler())
+	local d2=g:FilterCount(Card.IsHasEffect,nil,id)
+	local d3=g:FilterCount(Card.IsHasEffect,nil,id+1)
+	local ft=Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)
+	return (ft>-2 and #g>1 and d2>0) or (ft>-1 and d3>0)
+end
+function s.plasmaop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetReleaseGroup(tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local sg1=g:FilterSelect(tp,Card.IsHasEffect,1,1,nil,id)
+	if not sg1:GetFirst():IsHasEffect(id+1) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local sg2=g:Select(tp,1,1,sg1:GetFirst())
+		sg1:Merge(sg2)
+	end
+	Duel.Release(sg1,REASON_COST)
 end
