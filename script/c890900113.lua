d77f66b7dabdc7725a90a5950ac4035d824e8957
Radio2004 <Andrijsupa@gmail.com>
2022-07-02 20:57:00 +0300

    Update c890900113.lua

--- script/c890900113.lua
+++ script/c890900113.lua
@@ -5,14 +5,20 @@
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3dd),5,1)
 	c:EnableReviveLimit()
 		--summon proc
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(id)
-	c:RegisterEffect(e3)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(id)
+	c:RegisterEffect(e1)
 	aux.GlobalCheck(s,function()
 		--summon proc
+		local ge1=Effect.CreateEffect(c)
+		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge1:SetCode(EVENT_ADJUST)
+		ge1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		ge1:SetOperation(s.op)
+		Duel.RegisterEffect(ge1,0)
 		local ge1=Effect.CreateEffect(c)
 		ge1:SetDescription(aux.Stringid(id,0))
 		ge1:SetType(EFFECT_TYPE_FIELD)
@@ -27,6 +33,36 @@ end
 	s.listed_series={0x3dd}
 	function s.rfilter(c,tp)
 	return c:IsCode(890900113) and (c:IsControler(tp) or c:IsFaceup())
+end
+	function s.filter(c)
+	return c:IsCode(890900018) or c:IsCode(83965310)
+end
+	function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(s.filter,0,LOCATION_HAND,LOCATION_HAND,nil)
+	local tc=g1:GetFirst()
+	while tc do
+		if tc:GetFlagEffect(id)==0 then
+			local e2=Effect.CreateEffect(tc)
+			e2:SetDescription(aux.Stringid(64382841,3))
+			e2:SetType(EFFECT_TYPE_FIELD)
+			e2:SetCode(EFFECT_SPSUMMON_PROC)
+			e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+			e2:SetRange(LOCATION_HAND)
+			if tc:IsCode(890900018) then
+				e2:SetValue(1)
+				e2:SetCondition(s.sumcon)
+				e2:SetTarget(s.sumtg)
+				e2:SetOperation(s.sumop)
+			else
+				e2:SetCondition(s.plasmacon)
+				e2:SetOperation(s.plasmaop)
+			end
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e2)
+			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+		end
+		tc=g1:GetNext()
+	end
 end
 function s.sumcon(e,c)
 	if c==nil then return true end
@@ -49,4 +85,23 @@ function s.sumop(e,tp,eg,ep,ev,re,r,rp,c)
 	if not g then return end
 	Duel.Release(g,REASON_COST)
 	g:DeleteGroup()
+end
+	function s.plasmacon(e,c)
+	if c==nil then return true end
+	local g=Duel.GetReleaseGroup(c:GetControler())
+	local d2=g:FilterCount(Card.IsHasEffect,nil,id)
+	local d3=g:FilterCount(Card.IsHasEffect,nil,id+1)
+	local ft=Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)
+	return (ft>-2 and #g>1 and d2>0) or (ft>-1 and d3>0)
+end
+function s.plasmaop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.GetReleaseGroup(tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local sg1=g:FilterSelect(tp,Card.IsHasEffect,1,1,nil,id)
+	if not sg1:GetFirst():IsHasEffect(id+1) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+		local sg2=g:Select(tp,1,1,sg1:GetFirst())
+		sg1:Merge(sg2)
+	end
+	Duel.Release(sg1,REASON_COST)
 end
