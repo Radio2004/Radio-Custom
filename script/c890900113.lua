115ab67920ca06b385ca1d7f1c19b4222201e3e5
Radio2004 <Andrijsupa@gmail.com>
2022-07-03 14:42:06 +0300

    Update c890900113.lua

--- script/c890900113.lua
+++ script/c890900113.lua
@@ -9,7 +9,6 @@
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
 	c:RegisterEffect(e1)
 	aux.GlobalCheck(s,function()
 		local ge1=Effect.CreateEffect(c)
