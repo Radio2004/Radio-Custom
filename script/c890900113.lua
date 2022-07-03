21c46f184a3b6e2789be4ab150b1f00c7961ae45
Radio2004 <Andrijsupa@gmail.com>
2022-07-03 14:58:40 +0300

    Update c890900113.lua

--- script/c890900113.lua
+++ script/c890900113.lua
@@ -47,8 +47,6 @@ end
 			tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 			end
 		end
-		tc=g1:GetNext()
-	end
 function s.sumcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
