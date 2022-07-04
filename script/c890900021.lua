c196d87a8ad574d8c9077810dd199fe979c1a638
Radio2004 <Andrijsupa@gmail.com>
2022-07-04 11:45:28 +0300

    Update c890900021.lua

--- script/c890900021.lua
+++ script/c890900021.lua
@@ -42,7 +42,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 end
 	function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local cont,loc=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x1fa3) and cont==tp and loc~=LOCATION_MZONE
+	return re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x1fa3) and cont==tp and loc==LOCATION_MZONE
 end
 	function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chkc then return chkc:IsOnField() end
