87f79d3379529be24ec3973022e3779878e5b20c
Radio2004 <Andrijsupa@gmail.com>
2022-07-06 14:35:51 +0300

    Update c890900021.lua

--- script/c890900021.lua
+++ script/c890900021.lua
@@ -49,7 +49,7 @@ end
 end
 	function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=e:GetLabelObject()
-	if chk==0 then return tc:IsCanChangePosition() and tc:IsLocation(LOCATION_MZONE) and tc:IsCanBeEffectTarget(e) end
+	if chk==0 then return tc:IsCanChangePosition() and tc:IsLocation(LOCATION_MZONE) end
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
