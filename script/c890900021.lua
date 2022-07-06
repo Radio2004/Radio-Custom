39ec6ef6c586312df9a306c5ae6d2bcaecce6144
Radio2004 <Andrijsupa@gmail.com>
2022-07-06 16:55:36 +0300

    Update c890900021.lua

--- script/c890900021.lua
+++ script/c890900021.lua
@@ -47,10 +47,6 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 	function s.regop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
-	if not g or #g~=1 then return false end
-	local tc=g:GetFirst()
-	e:SetLabelObject(tc)
 	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
 end
 	function s.thcon(e,tp,eg,ep,ev,re,r,rp)
