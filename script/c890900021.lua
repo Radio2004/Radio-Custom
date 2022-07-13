9cf5cd5dbeac9f77708ff1c8b3e4e7584942dab3
Radio2004 <Andrijsupa@gmail.com>
2022-07-13 17:24:27 +0300

    Update c890900021.lua

--- script/c890900021.lua
+++ script/c890900021.lua
@@ -74,7 +74,7 @@ end
 --end
 	function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local tc=e:GetLabelObject()
-	if chk==0 then return true end
+	if chk==0 then return tc:IsCanChangePosition() end
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
