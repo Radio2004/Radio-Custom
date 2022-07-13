d6525a2ba8f304c691d4335768a742cfd3dd09f0
Radio2004 <Andrijsupa@gmail.com>
2022-07-13 16:49:15 +0300

    a

--- script/c890900021.lua
+++ script/c890900021.lua
@@ -37,8 +37,9 @@ s.listed_series={0x1fa3}
 	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
 	if not g or #g~=1 then return false end
 	local tc=g:GetFirst()
+	e:SetLabelObject(tc)
 	for tc in aux.Next(ev) do
-		Duel.RegisterFlagEffect(tc:SetLabelObject(),id,RESET_PHASE+PHASE_END,0,1)
+		Duel.RegisterFlagEffect(tc:GetControler(),id,RESET_PHASE+PHASE_END,0,1)
 	end
 end
 	function s.filter(c)
