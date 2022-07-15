ee3d2f448007857c34fbb3ab9314b039d155c652
Radio2004 <Andrijsupa@gmail.com>
2022-07-15 14:36:35 +0300

    Update c890900005.lua

--- script/c890900005.lua
+++ script/c890900005.lua
@@ -97,7 +97,7 @@ end
 		--battle indes
 	   local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_NO_TURN_RESET)
 		e1:SetRange(LOCATION_MZONE)
 		e1:SetCountLimit(1)
