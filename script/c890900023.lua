2bee5211302c00273e73707ac7a8000c881c1da3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-26 15:28:24 +0300

    Update c890900023.lua

--- script/c890900023.lua
+++ script/c890900023.lua
@@ -90,13 +90,13 @@ end
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(e:GetLabel(ct*200))
+		e1:SetValue(e:GetLabel()*200)
 		tc:RegisterEffect(e1)
 		end
 	else
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.schfilter,tp,LOCATION_DECK,0,e:GetLabel(ct),math.floor(e:GetLabel(ct)/2),nil)
+		local g=Duel.SelectMatchingCard(tp,s.schfilter,tp,LOCATION_DECK,0,e:GetLabel(ct),math.floor(e:GetLabel()/2),nil)
 		if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
