bbe0be6f5bdf059d4de0ca1cdf26f466861a2e5e
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-26 10:14:33 +0300

    Update c890900023.lua

--- script/c890900023.lua
+++ script/c890900023.lua
@@ -64,7 +64,7 @@ end
 end
 	function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local b1=e:GetLabel() > 0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
+	local b1=e:GetLabel(sum)>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
 	local b2=e:GetLabel() > 2
 	if chk==0 then return (b1 or b2) end
 	local op=0
