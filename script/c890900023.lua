a5c5f729c2c98b2859be7b0650b37c2c90ea7b0a
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-26 14:15:15 +0300

    Update c890900023.lua

--- script/c890900023.lua
+++ script/c890900023.lua
@@ -71,7 +71,9 @@ end
 	elseif op==1 then
 		e:SetLabel(ct*200)
 		Duel.RemoveCounter(tp,1,0,0x382,ct,REASON_COST)
+		Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE)
 	else
+		e:SetLabel(ct)
 		Duel.RemoveCounter(tp,1,0,0x382,ct,REASON_COST)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 	end
