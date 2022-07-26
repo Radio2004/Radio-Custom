00b9fa8cfb72a547d5962ef9369db9369eeddf13
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-26 15:52:30 +0300

    Update c890900023.lua

--- script/c890900023.lua
+++ script/c890900023.lua
@@ -97,7 +97,7 @@ end
 	else
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.schfilter,tp,LOCATION_DECK,0,e:GetLabel(),math.floor(e:GetLabel()/2),nil)
+		local g=Duel.SelectMatchingCard(tp,s.schfilter,tp,LOCATION_DECK,0,1,math.floor(e:GetLabel()/2),nil)
 		if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
