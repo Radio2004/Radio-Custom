44030e61551faac822048dd8fee1e3b899141bcb
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-26 16:14:47 +0300

    Update c890900023.lua

--- script/c890900023.lua
+++ script/c890900023.lua
@@ -65,7 +65,8 @@ end
 		{b1,aux.Stringid(id,0)},
 		{b2,aux.Stringid(id,1)},
 		{b3,aux.Stringid(id,2)})
-		e:SetLabel(op,ct)
+		e:SetLabel(op)
+		e:SetLabel(ct)
 	local g=(op==1 and g1 or g2 or g3)
 		Duel.RemoveCounter(tp,1,0,0x382,ct,REASON_COST)
 	end
