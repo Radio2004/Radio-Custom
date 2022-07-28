1513405d8c7a5268f0a9f397f3f0196424605c31
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-28 12:46:26 +0300

    Update c890900026.lua

--- script/c890900026.lua
+++ script/c890900026.lua
@@ -21,7 +21,7 @@ end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,3,min.floor(3),nil)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,3,nil)
 	Duel.ConfirmCards(1-tp,g)
 	Duel.ShuffleHand(tp)
 end
