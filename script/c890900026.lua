234a204b7895d998a821a16c493c70b67179dce6
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-28 13:41:38 +0300

    Update c890900026.lua

--- script/c890900026.lua
+++ script/c890900026.lua
@@ -42,8 +42,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local att=e:GetLabel()
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT) > 0 then
-	local b1=att(ATTRIBUTE_FIRE)
-	local b2=att(ATTRIBUTE_WATER)
+	local b1=att() == ATTRIBUTE_FIRE
+	local b2=att() == ATTRIBUTE_WATER
 	local dtab={}
 	if b1 then
 		table.insert(dtab,aux.Stringid(id,0))
