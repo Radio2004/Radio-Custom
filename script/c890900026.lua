567367af9d4d0d81ba8b4f041c7b1eeeb55f80e7
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-07-28 16:04:28 +0300

    Update c890900026.lua

--- script/c890900026.lua
+++ script/c890900026.lua
@@ -25,8 +25,8 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,3,nil) end
 	local ft=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_HAND,0,nil,0x1fa3)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,ft,nil)
-	e:SetLabel(g:GetFirst():GetAttribute())
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,3,ft,nil)
+	e:SetLabel(g:GetFirst())
 	Duel.ConfirmCards(1-tp,g)
 	Duel.ShuffleHand(tp)
 end
@@ -40,8 +40,8 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local dtab={}
-	local b1=e:GetLabel():IsAttribute(ATTRIBUTE_FIRE)
-	local b2=e:GetLabel():IsAttribute(ATTRIBUTE_WATER)
+	local b1=e:GetLabel():GetAttribute():IsAttribute(ATTRIBUTE_FIRE)
+	local b2=e:GetLabel():GetAttribute():IsAttribute(ATTRIBUTE_WATER)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT) > 0 then
 	if b1 then
 		table.insert(dtab,aux.Stringid(id,2))
