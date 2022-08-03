5dc4e79a65c36cdc2b30ae86c7773be05935bddb
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-03 11:27:41 +0300

    Update c890900026.lua

--- script/c890900026.lua
+++ script/c890900026.lua
@@ -26,7 +26,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetMatchingGroupCount(Card.IsSetCard,tp,LOCATION_HAND,0,nil,0x1fa3)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,3,60,nil)
-	e:SetLabel(g:GetFirst():GetAttribute())
+	Duel.SetTargetParam(g)
 	Duel.ConfirmCards(1-tp,g)
 	Duel.ShuffleHand(tp)
 end
@@ -40,8 +40,9 @@ end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local dtab={}
-	local b1=e:GetLabel(IsAttribute(ATTRIBUTE_FIRE))
-	local b2=e:GetLabel(IsAttribute(ATTRIBUTE_WATER))
+	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local b1=ac:IsAttribute(ATTRIBUTE_FIRE)
+	local b2=ac:IsAttribute(ATTRIBUTE_WATER)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT) > 0 then
 	if b1 then
 		table.insert(dtab,aux.Stringid(id,2))
