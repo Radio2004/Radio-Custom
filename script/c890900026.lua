e60b9bb31fbbf3517a4387cfe8b60f8fc1a55232
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-03 11:28:30 +0300

    Update c890900026.lua

--- script/c890900026.lua
+++ script/c890900026.lua
@@ -41,8 +41,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local dtab={}
 	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
-	local b1=ac:IsAttribute(ATTRIBUTE_FIRE)
-	local b2=ac:IsAttribute(ATTRIBUTE_WATER)
+	local b1=ac:IsAttribute(ATTRIBUTE_FIRE) > 0
+	local b2=ac:IsAttribute(ATTRIBUTE_WATER) > 0
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) and Duel.Destroy(eg,REASON_EFFECT) > 0 then
 	if b1 then
 		table.insert(dtab,aux.Stringid(id,2))
