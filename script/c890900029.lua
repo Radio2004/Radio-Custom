e5a436bfad26fb00713b7782913ea3cbf2326ca7
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-09 16:12:10 +0300

    a

---
+++ script/c890900029.lua
@@ -0,0 +1,34 @@
+--Flora de Fleur, Melirria's Skill Manager
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro summon
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x3dd),1,1,Synchro.NonTunerEx(Card.IsSetCard,0x3dd),1,99)
+	c:EnableReviveLimit()
+	--Destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+	function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+	local atk=tc:GetFirst():GetTextAttack()
+	if atk<0 then atk=0 end
+		if Duel.Destroy(tc:GetFirst(),REASON_EFFECT)~=0 then
+			Duel.Recover(tp,atk,REASON_EFFECT)
+		end
+	end
