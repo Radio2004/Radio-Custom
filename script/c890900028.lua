dfabc39e0eca29f1fd26626a27433396ca79cf4c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-09 11:26:28 +0300

    Update c890900028.lua

--- script/c890900028.lua
+++ script/c890900028.lua
@@ -21,7 +21,7 @@ end
 	function s.lcheck(g,lc,sumtype,tp)
 	return g:IsExists(Card.IsSetCard,1,nil,0x38d,lc,sumtype,tp)
 end
-	function s.thfilter(c,e,tp,code)
+	function s.thfilter(c,tp)
 	return c:IsSetCard(0x3dd) and c:IsType(TYPE_FIELD) and c:GetActivateEffect() and c:GetActivateEffect():IsActivatable(tp,true,true)
 end
   
@@ -49,6 +49,6 @@ end
 	function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,ct):GetFirst()
+	local tc=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,ct,tp):GetFirst()
 	aux.PlayFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
 end
