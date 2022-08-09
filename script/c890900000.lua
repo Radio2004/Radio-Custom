d7faa165043404fa78683bb65bf1b5ac0340f8b7
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-09 18:05:22 +0300

    Update c890900000.lua

--- script/c890900000.lua
+++ script/c890900000.lua
@@ -71,8 +71,7 @@ end
 		local tc=Duel.GetFirstTarget()
 		if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
 			Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
-	end
-		Duel.SpecialSummonComplete()
-end
+			Duel.SpecialSummonComplete()
 	end
 end
+	end
