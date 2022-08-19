7861f6b7b77adc54fffe9dd11d9434901392441f
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-19 20:29:14 +0300

    a

--- script/c444444443.lua
+++ script/c444444443.lua
@@ -5,7 +5,7 @@
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)	
 	e1:SetCountLimit(1,id)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
--- script/c444444444.lua
+++ script/c444444444.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)  
+	e1:SetProperty(EFFECT_FLAG_DELAY)  
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_NOVELLA)
@@ -22,7 +22,7 @@ function s.filter(c)
 	return c:IsFaceup() and c:IsSetCard(0x1BC)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g1=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
+	local g1=Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil)
 	local g2=Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil)
 	local b1=g1
 	local b2=g2
@@ -31,18 +31,24 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 		{b1,aux.Stringid(id,1)},
 		{b2,aux.Stringid(id,0)})
 	e:SetLabel(op)
-	local g=(op==1 and g1 or g2)   
+	if op==1 then
+		local g=Duel.GetMatchingGroup(aux.AND(s.filter,Card.IsCanBeEffectTarget),tp,LOCATION_MZONE,0,nil,e)
+		local att=aux.AnnounceAnotherAttribute(g,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local sel=g:FilterSelect(tp,Card.IsDifferentAttribute,1,1,nil,att)
+		Duel.SetTargetCard(sel)
+		e:SetLabel(att)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
 end
+	end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
-		local g=Duel.GetMatchingGroup(aux.AND(s.filter,Card.IsCanBeEffectTarget),tp,LOCATION_MZONE,0,nil,e)
-	local att=aux.AnnounceAnotherAttribute(g,tp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local sel=g:FilterSelect(tp,Card.IsDifferentAttribute,1,1,nil,att)
-	Duel.SetTargetCard(sel)
-	e:SetLabel(att)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local tc=Duel.GetFirstTarget()
+		if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
@@ -52,16 +58,13 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 		tc:RegisterEffect(e1)
 		end
 	else
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)   
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local c=e:GetHandler()
 		local tc=Duel.GetFirstTarget()
-		if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetValue(math.ceil(tc:GetAttack()/2))
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(tc:GetAttack()/2)
 		tc:RegisterEffect(e1)
 	end
 end
