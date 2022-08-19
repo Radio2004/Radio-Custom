b2e1b5c6708aed19a10345dd9a7c900e0c7b35d8
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-19 21:26:26 +0300

    Update c444444444.lua

--- script/c444444444.lua
+++ script/c444444444.lua
@@ -30,7 +30,6 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local op=aux.SelectEffect(tp,
 		{b1,aux.Stringid(id,0)},
 		{b2,aux.Stringid(id,1)})
-		e:SetLabel(op,att)
 	if op==1 then
 		local g=Duel.GetMatchingGroup(aux.AND(s.filter,Card.IsCanBeEffectTarget),tp,LOCATION_MZONE,0,nil,e)
 		local att=aux.AnnounceAnotherAttribute(g,tp)
@@ -43,6 +42,7 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
 		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	end
+e:SetLabel(op,att)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local op,att=e:GetLabel()
