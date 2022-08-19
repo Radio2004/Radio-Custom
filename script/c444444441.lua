7b6a39e22bec0e29056ac94b5a1c5ae0069d49c3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-19 18:52:48 +0300

    Update c444444441.lua

--- script/c444444441.lua
+++ script/c444444441.lua
@@ -4,7 +4,7 @@ function s.initial_effect(c)
 	--effect
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
@@ -21,8 +21,8 @@ function s.schfilter(c)
 	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSummon(tp)
 	local g2=Duel.IsExistingMatchingCard(s.schfilter,tp,LOCATION_DECK,0,1,nil) 
-	local g1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0   
 	local b1=g1
 	local b2=g2   
 	if chk==0 then return b1 or b2 end
@@ -30,8 +30,10 @@ function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 		{b1,aux.Stringid(id,0)},
 		{b2,aux.Stringid(id,1)})
 	e:SetLabel(op)
-	local g=(op==1 and g1 or g2)
-end 
+	if op==2 then
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+	end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==1 then
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -39,11 +41,11 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetDescription(aux.Stringid(id,2))
 		e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
 		e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-		e1:SetTarget(s.exstg)
+		e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1bc))
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		Duel.RegisterEffect(e1,tp)
-		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 	else
+		if not e:GetHandler():IsRelateToEffect(e) then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.SelectMatchingCard(tp,s.schfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if #g>0 then
@@ -51,7 +53,4 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 			Duel.ConfirmCards(1-tp,g)
 		end
 	end
-end
-function s.exstg(e,c)
-	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER)
 end
