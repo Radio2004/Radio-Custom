26196510e07fba4c5960134b9499c46f298d1f2e
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-19 14:46:20 +0300

    Update c889900004.lua

--- script/c889900004.lua
+++ script/c889900004.lua
@@ -73,7 +73,6 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 	Duel.SpecialSummonComplete()
 	end
-end
 	local e3=Effect.CreateEffect(e:GetHandler())
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
