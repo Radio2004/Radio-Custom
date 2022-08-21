53c3f24d4bc597febf3ea097a895b1b846bcab96
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-21 16:34:55 +0300

    Update c444444445.lua

--- script/c444444445.lua
+++ script/c444444445.lua
@@ -13,23 +13,35 @@ function s.initial_effect(c)
 end
 s.listed_series={0x1BC}
 function s.cfilter(c)
-	return c:IsSetCard(0x1BC) and c:IsAbleToHand()and c:IsType(TYPE_MONSTER)
-end
-function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x1BC) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil) end 
-	local g=Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil)
-	Duel.SendtoHand(g,nil,REASON_EFFECT)
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local k=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND,0,nil,e,tp)
-	if #k~=0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND,0,1,1,nil,e,tp)
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
-	end
+	return c:IsSetCard(0x1BC) and c:IsAbleToHand() and c:IsType(TYPE_MONSTER)
 end
 
+
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x1BC) and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
+end
+
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.cfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,#g,0,LOCATION_GRAVE+LOCATION_MZONE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 then
+	local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,LOCATION_HAND,0,nil,e,tp)
+	if #g>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+	Duel.BreakEffect()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=g:Select(tp,1,1,nil):GetFirst()
+	if sc then
+		Duel.SpecialSummon(sc,0,tp,tp,true,false,POS_FACEUP)
+			end
+		end
+	end
+end
