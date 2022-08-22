240635cfdf9543326450b9e454b87154913204b3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-22 11:41:08 +0300

    Update c889900008.lua

--- script/c889900008.lua
+++ script/c889900008.lua
@@ -92,7 +92,8 @@ function s.tgfilter(c)
 end
 
 function s.spfilter(c,e,tp,mc)
-	return c:IsType(TYPE_LINK) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_LINK,tp,false,false) and c:IsSetCard(0x5eb) and c:IsLinkBelow(3)
+	return c:IsType(TYPE_LINK) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_LINK,tp,false,false) and c:IsSetCard(0x5eb) and c:IsLinkBelow(3) and not c:IsPublic()
+
 end
 
 
