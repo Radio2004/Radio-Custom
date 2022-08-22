2d31adf63983b083265268c3dd3357e67ea4b0ae
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-22 20:27:42 +0300

    Update c444444446.lua

--- script/c444444446.lua
+++ script/c444444446.lua
@@ -50,7 +50,7 @@ s.listed_names={444444447}
 		local tc=Duel.GetFirstTarget()
 		if not c:IsRelateToEffect(e) then return end
 		if tc and tc:IsRelateToEffect(e) then
-		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+	
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
