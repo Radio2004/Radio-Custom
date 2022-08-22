615b025783bb942b61d264c99b7db7fff0b1088c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-23 00:12:23 +0300

    Update c889900009.lua

--- script/c889900009.lua
+++ script/c889900009.lua
@@ -17,6 +17,15 @@ function s.initial_effect(c)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
+	--If used as a link material
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BE_MATERIAL)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(s.matcon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetOperation(s.matop)
+	c:RegisterEffect(e2)
 end
 s.listed_series={0x5eb}
 	function s.matfilter(c,scard,sumtype,tp)
@@ -53,5 +62,25 @@ end
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
+
+	function s.matcon(e,tp,eg,ep,ev,re,r,rp)
+	return r & REASON_LINK == REASON_LINK and e:GetHandler():GetReasonCard():IsLinkMonster() and c:IsSetCard(0x5eb)
+end
+
+	function s.matop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local sc=c:GetReasonCard()
+	if sc and sc:IsFaceup() and sc:IsOnField() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3061)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		sc:RegisterEffect(e1)
 	end
 end
