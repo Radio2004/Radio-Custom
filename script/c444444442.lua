62824b27fb901694c7b79de3a34a256e3d59205b
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-22 23:13:26 +0300

    Update c444444442.lua

--- script/c444444442.lua
+++ script/c444444442.lua
@@ -64,11 +64,13 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e:SetCategory(CATEGORY_TOHAND)
    else
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e:SetCategory(CATEGORY_SPECIAL_SUMMON)
 end
 	end
 
