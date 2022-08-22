52f813d76f82eee5ea5b1d9eee68663ffa9bb991
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-22 12:01:02 +0300

    Update c889900008.lua

--- script/c889900008.lua
+++ script/c889900008.lua
@@ -92,16 +92,13 @@ function s.tgfilter(c)
 end
 
 function s.spfilter(c,e,tp,mc)
-	local lv=c:GetLink()
 	return c:IsType(TYPE_LINK) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0 and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_LINK,tp,false,false) and c:IsSetCard(0x5eb) and c:IsLinkBelow(3)
 end
 
 
 
 	function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then 
-	local pg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.tgfilter),tp,LOCATION_REMOVED,0,c)
-	return #pg<=0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 	Duel.ConfirmCards(1-tp,g)
