cbefc705663648beb78fb5e3afab0e3a577aaded
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-22 19:58:37 +0300

    Update c444444446.lua

--- script/c444444446.lua
+++ script/c444444446.lua
@@ -69,8 +69,8 @@ s.listed_names={444444447}
 		e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
 		e3:SetReset(RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e3)
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+		if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP_DEFENSE)~=0 then
+		c:CompleteProcedure()
 			end
 		end 
 	end
