5f0c76bb9ac0ddc72a88140a43f17f69209f4308
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-22 09:46:51 +0300

    Update c889900008.lua

--- script/c889900008.lua
+++ script/c889900008.lua
@@ -17,7 +17,18 @@ function s.initial_effect(c)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsLinked))
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
 end
+
+
 	function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
