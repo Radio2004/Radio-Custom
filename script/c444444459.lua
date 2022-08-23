56f21c342f2cf593f6f2c3e79b2f1cb087c8735b
Radio2004 <Andrijsupa@gmail.com>
2022-08-23 22:35:13 +0300

    Update c444444459.lua

--- script/c444444459.lua
+++ script/c444444459.lua
@@ -5,8 +5,7 @@ function s.initial_effect(c)
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x1BC),2,2)
  -- Effects
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_RECOVER+CATEGORY_TOGRAVE)
+	e1:SetDescription(aux.Stringid(id,3))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
@@ -15,14 +14,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.filter(c)
-	return c:IsSetCard(0x1BC)and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
+	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 	end
-	function s.filter1(c)
-	return c:IsFaceup() and c:GetBaseAttack()
+	function s.recfilter(c)
+	return c:IsFaceup() and c:GetBaseAttack()>0
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g1=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil)
-	local g2=Duel.IsExistingTarget(s.filter1,tp,0,LOCATION_MZONE,1,nil)
+	local g2=Duel.IsExistingTarget(s.recfilter,tp,0,LOCATION_MZONE,1,nil)
 	local b1=g1
 	local b2=g2
 	if chk==0 then return b1 or b2 end
@@ -30,21 +29,27 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 		{b1,aux.Stringid(id,0)},
 		{b2,aux.Stringid(id,1)})
 	e:SetLabel(op)
-	local g=(op==1 and g1 or g2)
+	if op==1 then
+		e:SetCategory(CATEGORY_TOGRAVE)
+		Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	else
+		e:SetCategory(CATEGORY_RECOVER)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		local g=Duel.SelectTarget(tp,s.recfilter,tp,0,LOCATION_MZONE,1,1,nil)
+		Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,g:GetFirst():GetBaseAttack())
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
 	if e:GetLabel()==1 then
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
-		Duel.SendtoGrave(g,REASON_EFFECT)end
-	else
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.filter1,tp,0,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,g:GetFirst():GetAttack())
-	local tc=Duel.GetFirstTarget(tp,0)
-	if tc:IsRelateToEffect(e) and tc:IsFaceup()and tc:IsControler(1-tp) and tc:GetBaseAttack() then
-		Duel.Recover(tp,tc:GetBaseAttack(),REASON_EFFECT)
-	end
+		Duel.SendtoGrave(g,REASON_EFFECT)
 end
+	else
+	if e:GetHandler():IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		Duel.Recover(tp,tc:GetBaseAttack(),REASON_EFFECT)
+		end
+	end
 end
