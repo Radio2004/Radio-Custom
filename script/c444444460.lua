383cb512738d289e204fc3c3b1b410d6c62fbc2f
Radio2004 <Andrijsupa@gmail.com>
2022-08-23 23:29:24 +0300

    a

--- script/c444444460.lua
+++ script/c444444460.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x1BC),2,2)
 	 -- Effects
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(id,2))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_FREE_CHAIN)
