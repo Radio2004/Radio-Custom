fd5b8e158f1c1374fad2f90a7847f29e7a67a7f6
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-23 11:16:40 +0300

    Update c889900011.lua

--- script/c889900011.lua
+++ script/c889900011.lua
@@ -54,33 +54,48 @@ function s.gyop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SendtoGrave(g,REASON_EFFECT)
 end
 
+	function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(1)
+	return true
+end
+
 	function s.costfilter(c)
 	return c:IsSetCard(0x5eb) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
 end
 
-	function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ct=Duel.GetMatchingGroupCount(Card.IsType,tp,0,LOCATION_GRAVE,nil,TYPE_MONSTER)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) and ct>0 end
-	if ct>2 then ct=2 end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,2,nil)
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
-	e:SetLabel(#g)
+	function s.remfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemove() 
 end
 
-	function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return true end
-	local g=Duel.GetMatchingGroup(Card.IsType,tp,0,LOCATION_GRAVE,nil,TYPE_MONSTER)
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,e:GetLabel(),0,0)
+function s.rescon(sg,e,tp,mg)
+	local ct=#sg
+	sg:AddCard(e:GetHandler())
+	local res=Duel.SelectMatchingCard(s.remfilter,tp,0,LOCATION_GRAVE,ct,sg)
+	sg:RemoveCard(e:GetHandler())
+	return res
 end
 
-	function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local ct=e:GetLabel()
-	local g=Duel.GetMatchingGroup(Card.IsType,tp,0,LOCATION_GRAVE,nil,TYPE_MONSTER)
-	if #g<ct then return end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local rg=Duel.GetMatchingGroup(s.costfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
+	if chk==0 then
+		if e:GetLabel()==1 then
+			e:SetLabel(0)
+			return aux.SelectUnselectGroup(rg,e,tp,nil,2,s.rescon,0)
+		else return false end
+	end
+
+	e:SetLabel(0)
+	local rsg=aux.SelectUnselectGroup(rg,e,tp,nil,2,s.rescon,1,tp,HINTMSG_REMOVE,s.rescon)
+	local ct=Duel.Remove(rsg,POS_FACEUP,REASON_COST)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local sg=g:Select(tp,ct,ct,nil)
-	Duel.HintSelection(sg)
+	local g=Duel.SelectTarget(tp,s.remfilter,tp,0,LOCATION_GRAVE,ct,ct,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
 	Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
 end
 
