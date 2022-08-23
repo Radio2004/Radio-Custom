83af72f4669fc6eea36b27fc67e987b4e97fecf4
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-08-23 10:16:00 +0300

    a

---
+++ script/c889900011.lua
@@ -0,0 +1,33 @@
+--ERr0r Wall Breaker
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x5eb),2)
+	--Return monsters from field/GY to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_NO_TURN_RESET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e1:SetTarget(s.gytg)
+	e1:SetOperation(s.gyop)
+	c:RegisterEffect(e1)
+end
+   
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	local ct=#(c:GetMutualLinkedGroup():Filter(Card.IsType,nil,TYPE_MONSTER))
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and s.thfilter(chkc) end
+	if chk==0 then return ct>0 and Duel.IsExistingTarget(Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToGrave,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,0,0)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	Duel.SendtoGrave(g,REASON_EFFECT)
+end
