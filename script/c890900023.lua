67d1448462797cb236294d1518956a690d2045b0
Radio2004 <Andrijsupa@gmail.com>
2022-08-23 17:35:53 +0300

    Update c890900023.lua

--- script/c890900023.lua
+++ script/c890900023.lua
@@ -16,6 +16,17 @@
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
+	--Remove counters
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCost(s.spcost)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
 end
 s.counter_place_list={0x382}
 	function s.lcheck(g,lc,sumtype,tp)
@@ -34,4 +45,35 @@ function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():AddCounter(0x382,1)
+end
+   function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	c:RemoveCounter(tp,0x382,c:GetCounter(0x382),REASON_COST)
+end
+	function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local ct=c:GetCounter(0x382)
+	local b1=Duel.RemoveCounter(tp,1,0,0x382,5,REASON_COST)
+	local b2=Duel.IsCanRemoveCounter(tp,1,0,0x382,7,REASON_COST)
+		and Duel.IsExistingMatchingCard(s.rmfilter,tp,0,LOCATION_ONFIELD+LOCATION_GRAVE,1,nil)
+	if chk==0 then return ct>0 and (b1 or b2) end
+	local op=0
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1))
+	elseif b1 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,0))
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(id,1))+1
+	end
+	e:SetLabel(op)
+	if op==0 then
+		e:SetCategory(CATEGORY_DAMAGE)
+		Duel.RemoveCounter(tp,1,0,0x35,5,REASON_COST)
+		Duel.SetTargetPlayer(1-tp)
+		Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*300)
+	else
+		e:SetCategory(CATEGORY_REMOVE)
+		Duel.RemoveCounter(tp,1,0,0x35,7,REASON_COST)
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,0,LOCATION_ONFIELD+LOCATION_GRAVE)
+	end
 end
