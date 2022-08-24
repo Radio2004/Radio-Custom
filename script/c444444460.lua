ab1bb8a2d95920a84425d5b9e9b2f099d5150a9d
Radio2004 <Andrijsupa@gmail.com>
2022-08-24 20:35:51 +0300

    Update c444444460.lua

--- script/c444444460.lua
+++ script/c444444460.lua
@@ -90,7 +90,7 @@ end
 
 function s.bancon(rg,e,tp,eg,ep,ev,re,r,rp)
    local c=e:GetHandler()
-   if c:IsDisabled() then
+   if c:IsDisabled() or s.desop==false then
    return true
    else
    return false
