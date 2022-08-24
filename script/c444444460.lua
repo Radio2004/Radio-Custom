ff90c7d1211494dc1207b296b19ede51e39ec604
Radio2004 <Andrijsupa@gmail.com>
2022-08-24 17:59:06 +0300

    Update c444444460.lua

--- script/c444444460.lua
+++ script/c444444460.lua
@@ -91,7 +91,7 @@ end
 
 function s.bancon(rg,e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc:e:GetLabelObject()
+	local tc=e:GetLabelObject()
 	if c:IsDisabled() or not tc:IsLocation(LOCATION_REMOVED) then
 		return true
 	else
