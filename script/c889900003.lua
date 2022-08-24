1513eef848dd55007799178123c988633da943d5
Radio2004 <Andrijsupa@gmail.com>
2022-08-24 18:43:01 +0300

    Update c889900003.lua

--- script/c889900003.lua
+++ script/c889900003.lua
@@ -15,7 +15,7 @@
 	--destroy spell/trap card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY)
+	e2:SetCategory(CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,{id,1})
