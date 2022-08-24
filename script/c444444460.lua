2d689e7879bf4f97c5654cdfe4a03ecd1d9f666e
Radio2004 <Andrijsupa@gmail.com>
2022-08-24 19:37:05 +0300

    Update c444444460.lua

--- script/c444444460.lua
+++ script/c444444460.lua
@@ -81,11 +81,9 @@ end
 	local rs=Group.FromCards(ct,oc)
 	Duel.BreakEffect()
 	if ct:IsLocation(LOCATION_REMOVED) then
-		aux.RemoveUntil(rs,POS_FACEDOWN,REASON_EFFECT,PHASE_END,id,e,tp,function(rg) Duel.SendtoHand(rg,nil,REASON_EFFECT) end)
-		e:SetLabelObject(oc)
+		aux.RemoveUntil(rs,POS_FACEDOWN,REASON_EFFECT,PHASE_END,id,e,tp,function(rg) Duel.SendtoHand(rg,nil,REASON_EFFECT) end)   
 	else
 	   aux.RemoveUntil(oc,POS_FACEDOWN,REASON_EFFECT,PHASE_END,id,e,tp,function(rg) Duel.SendtoHand(rg,nil,REASON_EFFECT) end)
-	   e:SetLabelObject(oc)
 		end
 	end
 end
