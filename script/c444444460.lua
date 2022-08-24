dcda66ac9e6bc5f0da8797d43cf5a578b0eb67f2
Radio2004 <Andrijsupa@gmail.com>
2022-08-24 18:26:43 +0300

    Update c444444460.lua

--- script/c444444460.lua
+++ script/c444444460.lua
@@ -86,13 +86,13 @@ end
 	   aux.RemoveUntil(oc,POS_FACEDOWN,REASON_EFFECT,PHASE_END,id,e,tp,function(rg) Duel.SendtoHand(rg,nil,REASON_EFFECT) end)
 		end
 	end
-   e:SetLabelObject(ct)
+   e:SetLabelObject(oc)
 end
 
 function s.bancon(rg,e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=e:GetLabelObject()
-	if c:IsDisabled() or tc:IsLocation(LOCATION_REMOVED) then
+	if c:IsDisabled() or not tc:IsLocation(LOCATION_REMOVED) then
 		return true
 	else
 		return false
