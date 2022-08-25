58702a097dabe666aa370b4e52dfd7e90ed27a3f
Radio2004 <Andrijsupa@gmail.com>
2022-08-25 19:12:07 +0300

    Update c444444464.lua

--- script/c444444464.lua
+++ script/c444444464.lua
@@ -1,4 +1,61 @@
 --Harumi Mikoto, Novella Girl
-function c444444464.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	--effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_NOVELLA)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x1BC}
+function s.schfilter(c)
+	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+
+
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSummon(tp)
+	local g2=Duel.IsExistingMatchingCard(s.schfilter,tp,LOCATION_DECK,0,1,nil) 
+	local b1=g1
+	local b2=g2   
+	if chk==0 then return b1 or b2 end
+	local op=aux.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	e:SetLabel(op)
+	if op==2 then
+		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+	end
+
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetDescription(aux.Stringid(id,2))
+		e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
+		e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+		e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1bc))
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	else
+		if not e:GetHandler():IsRelateToEffect(e) then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.schfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
 end
