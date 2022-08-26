e1176d691d203464604cc5ab53f47ff8dc81f99f
Radio2004 <Andrijsupa@gmail.com>
2022-08-26 19:46:12 +0300

    Update c444444467.lua

--- script/c444444467.lua
+++ script/c444444467.lua
@@ -27,7 +27,23 @@ function s.initial_effect(c)
 	local e4=e3:Clone()
 	e4:SetCode(EFFECT_UPDATE_DEFENSE)
 	c:RegisterEffect(e4)
+	--effects
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,4))
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DELAY)
+	e5:SetCode(EVENT_SUMMON_SUCCESS)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,id)
+	e5:SetCondition(s.spcon)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+	local e6=e5:Clone()
+	e6:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e6)
 end
+s.listed_series={0x1bc}
 	function s.tg(e,c,tp)
 	if not c:IsSetCard(0x1BC) then return false end
 	if c:GetFlagEffect(1)==0 then
@@ -49,4 +65,56 @@ end
 
 	function s.val(e,c)
 	return Duel.GetMatchingGroupCount(aux.FilterFaceupFunction(Card.IsSetCard,0x1BC),e:GetHandler():GetControler(),LOCATION_MZONE,0,nil)*100
+end
+		function s.filter1(c)
+	return c:IsFaceup()and c:IsSetCard(0x1BC)
+	end
+
+	function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.filter1,1,nil)
+end
+
+	function s.filter(c)
+	return c:IsSetCard(0x1bc) and c:IsAbleToDeck()
+end
+
+	function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g1=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil)
+	local g2=Duel.IsExistingTarget(s.ctfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingTarget(Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,nil) 
+	local b1=g1
+	local b2=g2
+	if chk==0 then return b1 or b2 end
+	local op=aux.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	e:SetLabel(op)
+	if op==1 then
+		local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil)
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+		else
+		e:SetCategory(CATEGORY_TOHAND)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local g=Duel.SelectTarget(tp,s.ctfilter,tp,LOCATION_MZONE,0,1,1,nil)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_ONFIELD)
+	end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==1 then
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local sg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	if #sg>0 then
+		Duel.HintSelection(sg)
+		Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+end
+	else
+	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)~=0 and tc:IsLocation(LOCATION_HAND) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,0,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			end
+		end
+	end
 end
