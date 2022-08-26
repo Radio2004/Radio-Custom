d885a74cf0a142ad37c1974564d2b759c8f66206
Radio2004 <Andrijsupa@gmail.com>
2022-08-26 08:58:33 +0300

    Update c444444464.lua

--- script/c444444464.lua
+++ script/c444444464.lua
@@ -17,13 +17,15 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 s.listed_series={0x1BC}
-function s.schfilter(c)
-	return c:IsSetCard(0x1BC) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x1BC) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
 
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsPlayerCanSummon(tp)
+	local g1=Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0   
 	local g2=Duel.IsExistingMatchingCard(s.schfilter,tp,LOCATION_DECK,0,1,nil) 
 	local b1=g1
 	local b2=g2   
@@ -32,6 +34,12 @@ function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 		{b1,aux.Stringid(id,0)},
 		{b2,aux.Stringid(id,1)})
 	e:SetLabel(op)
+	if op==1 then
+	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 	if op==2 then
 		e:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
@@ -40,15 +48,33 @@ end
 
 
 function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
 	if e:GetLabel()==1 then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetDescription(aux.Stringid(id,2))
-		e1:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
-		e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-		e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x1bc))
-		e1:SetReset(RESET_PHASE+PHASE_END)
-		Duel.RegisterEffect(e1,tp)
+		if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+		--Banish it if it leaves the field
+		local e3=Effect.CreateEffect(c)
+		e3:SetDescription(3300)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e3:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		e3:SetValue(LOCATION_REMOVED)
+		tc:RegisterEffect(e3,true)
+		local e4=e3:Clone()
+		c:RegisterEffect(e4)
+	end
+	Duel.SpecialSummonComplete()
 	else
 		if not e:GetHandler():IsRelateToEffect(e) then return end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
