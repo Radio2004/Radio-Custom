4de1349fe54830f1147b16fbe57e4ba9848faf8d
Radio2004 <Andrijsupa@gmail.com>
2022-08-28 18:09:16 +0300

    Update c444444470.lua

--- script/c444444470.lua
+++ script/c444444470.lua
@@ -70,7 +70,7 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if e:GetLabel()==1 then
 	local lg=e:GetHandler():GetLinkedGroup()
-	local tc=Duel.IsExistingMatchingCard(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,lg)
+	local tc=Duel.GetMatchingGroupCount(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,lg)
 		if c:IsRelateToEffect(e) and c:IsFaceup() then
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
