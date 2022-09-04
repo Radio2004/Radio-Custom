d88ebb53cb90db4b18e53beb631f5b6746f9d6a5
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-09-04 11:55:22 +0300

    a

---
+++ script/c889900015.lua
@@ -0,0 +1,11 @@
+--Pandora's ERr0r Traitor, Codec Deletor
+flocal s,id=GetID()
+function s.initial_effect(c)
+	--link summon
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_CYBERSE),2,4,s.lcheck)
+	c:EnableReviveLimit()
+end
+	function s.lcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsType,1,nil,TYPE_LINK,lc,sumtype,tp)
+end
+
