3b70b0adde63335b05c0f16a555c4956f3852ae6
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-09-30 21:04:59 +0300

    Update c890900033.lua

--- script/c890900033.lua
+++ script/c890900033.lua
@@ -4,12 +4,24 @@ function s.initial_effect(c)
 	--synchro summon
 	Synchro.AddProcedure(c,s.tfilter,1,1,s.sfilter,1,1)
 	c:EnableReviveLimit()
-	--atk up
+	--destroy
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(s.atkval)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
 end
 s.material={890900035,890900036}
 s.listed_names={890900035,890900036,890900017,890900025,890900034}
@@ -23,4 +35,26 @@ function s.sfilter(c,scard,sumtype,tp)
 end
 function s.atkval(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,0x3dd)*200
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemoveAsCost,tp,LOCATION_GRAVE,0,1,nil) end
+	local rt=Duel.GetTargetCount(nil,tp,0,LOCATION_ONFIELD,nil)
+	if rt>2 then rt=2 end
+	local cg=Duel.DiscardHand(tp,Card.IsAbleToRemoveAsCost,1,rt,REASON_COST+REASON_DISCARD,nil)
+	e:SetLabel(cg)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,0,LOCATION_ONFIELD,1,nil)end
+	local ct=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,0,LOCATION_ONFIELD,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,ct,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local rg=tg:Filter(Card.IsRelateToEffect,nil,e)
+	if #rg>0 then
+		Duel.Destroy(rg,REASON_EFFECT)
+	end
 end
