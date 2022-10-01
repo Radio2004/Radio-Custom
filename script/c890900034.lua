53a23d7170cbd0d6d4de07687d88810fd8e7fa0e
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-01 14:30:43 +0300

    Update c890900034.lua

--- script/c890900034.lua
+++ script/c890900034.lua
@@ -1,6 +1,7 @@
 --Power of Melirria EX - Virus Dragon
 local s,id=GetID()
 function s.initial_effect(c)
+	c:SetUniqueOnField(1,1,id)
 	c:EnableReviveLimit()
 	--cannot special summon
 	local e1=Effect.CreateEffect(c)
@@ -8,4 +9,22 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e3)
+end
+s.listed_names={890900033}
+function s.atkfilter(c)
+	return c:IsSetCard(0x3de) and c:IsMonster() and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+end
+function s.atkval(e,c)
+	return Duel.GetMatchingGroupCount(s.atkfilter,c:GetControler(),LOCATION_MZONE+LOCATION_GRAVE,0,nil)*500
 end
