b4a57f7337d9c150316c16abf774d3eb4558e6c7
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-01 22:48:44 +0300

    Update c890900034.lua

--- script/c890900034.lua
+++ script/c890900034.lua
@@ -112,7 +112,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(cg)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		ac:RegisterEffect(e1)
 		ac=g:GetNext()
 	end
