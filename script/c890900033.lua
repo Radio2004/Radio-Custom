d7de0f26dcf22d6812f36caf8f803689beaf91e9
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 23:24:39 +0300

    Update c890900033.lua

--- script/c890900033.lua
+++ script/c890900033.lua
@@ -41,8 +41,8 @@ s.listed_series={0x3dd}
 function s.tfilter(c,scard,sumtype,tp)
 	local g=c:IsSummonCode(scard,sumtype,tp,890900035) or c:IsHasEffect(890900042)
 	return g
-	e:SetLabel(g)
 end
+e:SetLabel(g)
 function s.sfilter(c,scard,sumtype,tp)
 	local d=e:GetLabel()
 	if d==c:IsHasEffect(890900042) then
