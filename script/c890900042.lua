6dc104342b051c8a572fe16b14ec17acf528161d
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 20:38:31 +0300

    a

---
+++ script/c890900042.lua
@@ -0,0 +1,20 @@
+--Philosopher of Melirria - Yoko Hosino
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion substitute
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_FUSION_SUBSTITUTE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCondition(s.subcon)
+	e2:SetValue(s.subval)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x3dd}
+function s.subcon(e)
+	return e:GetHandler():IsLocation(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
+end
+function s.subval(e,c)
+	return c:IsSetCard(0x3dd)
+end
+
