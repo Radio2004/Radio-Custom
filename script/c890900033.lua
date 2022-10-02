796aa8dafb09063fb5955fd002f4f48a3a2d5562
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 21:14:02 +0300

    Update c890900033.lua

--- script/c890900033.lua
+++ script/c890900033.lua
@@ -48,7 +48,6 @@ end
 function s.sfilter(c,scard,sumtype,tp)
 	return c:IsSummonCode(scard,sumtype,tp,890900036) or (c:IsHasEffect(890900032) and not c:IsType(TYPE_TUNER))
 	end
-end
 function s.atkval(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,0x3dd)*200
 end
