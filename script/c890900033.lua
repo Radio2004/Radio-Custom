fcb113e0874c14d5bf91f91cc3a10c47616ad93c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 21:55:23 +0300

    a

--- script/c890900033.lua
+++ script/c890900033.lua
@@ -39,12 +39,19 @@ s.listed_names={890900035,890900036,890900017,890900025,890900034}
 s.material_setcode=0x3dd
 s.listed_series={0x3dd}
 function s.tfilter(c,scard,sumtype,tp)
-	return c:IsSummonCode(scard,sumtype,tp,890900035) or c:IsHasEffect(890900042)
+	if c:IsHasEffect(890900042,scard,sumtype,tp) then
+	return c:IsSummonCode(scard,sumtype,tp,890900035)
+	else
+	return c:IsSummonCode(scard,sumtype,tp,890900035) or (c:IsHasEffect(890900032) and c:IsType(TYPE_TUNER))
 	end
+end
 function s.sfilter(c,scard,sumtype,tp)
-	
-	return c:IsSummonCode(scard,sumtype,tp,890900036) or c:IsHasEffect(890900042)
+	if c:IsHasEffect(890900042,scard,sumtype,tp) then
+	return c:IsSummonCode(scard,sumtype,tp,890900036)
+	else
+	return c:IsSummonCode(scard,sumtype,tp,890900036) or (c:IsHasEffect(890900032) and not c:IsType(TYPE_TUNER))
 	end
+end
 function s.atkval(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsSetCard,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,0x3dd)*200
 end
