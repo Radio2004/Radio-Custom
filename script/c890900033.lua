4347ee6f71b674ea2b6e88bf0199226c63695922
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 22:19:38 +0300

    Update c890900033.lua

--- script/c890900033.lua
+++ script/c890900033.lua
@@ -40,14 +40,14 @@ s.material_setcode=0x3dd
 s.listed_series={0x3dd}
 function s.tfilter(c,scard,sumtype,tp)
 	if c:IsHasEffect(890900042,scard,sumtype,tp) then
-	return c:IsSummonCode(scard,sumtype,tp,890900035) or (c:IsHasEffect(890900032) and c:IsType(TYPE_TUNER))
+	return c:IsSummonCode(scard,sumtype,tp,890900035)
 	else
 	return c:IsSummonCode(scard,sumtype,tp,890900035) or (c:IsHasEffect(890900032) and c:IsType(TYPE_TUNER))
 	end
 end
 function s.sfilter(c,scard,sumtype,tp)
 	if c:IsHasEffect(890900042,scard,sumtype,tp) then
-	return c:IsSummonCode(scard,sumtype,tp,890900036) or (c:IsHasEffect(890900032) and not c:IsType(TYPE_TUNER))
+	return c:IsSummonCode(scard,sumtype,tp,890900036)
 	else
 	return c:IsSummonCode(scard,sumtype,tp,890900036) or (c:IsHasEffect(890900032) and not c:IsType(TYPE_TUNER))
 	end
