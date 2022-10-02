65eaf3be4d4258e5659ac03b8685dbcff5fff239
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 15:31:45 +0300

    a

---
+++ script/c890900039.lua
@@ -0,0 +1,10 @@
+--Melirria - Crimson Approaching!
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+end
