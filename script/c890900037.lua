124dfabce12196c3c58abe9ce51c4b3bb9041092
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 14:37:14 +0300

    Update c890900037.lua

--- script/c890900037.lua
+++ script/c890900037.lua
@@ -41,6 +41,6 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
-		Duel.SendtoDeck(tc,nil,REASON_EFFECT)
+		Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)
 	end
 end
