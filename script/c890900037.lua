f3852435a9d97589708f6563f5cccd83fa773565
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 14:32:33 +0300

    Update c890900037.lua

--- script/c890900037.lua
+++ script/c890900037.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	--return to a hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetCategory(CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetRange(LOCATION_MZONE)
@@ -41,6 +41,6 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
-		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.SendtoDeck(tc,nil,REASON_EFFECT)
 	end
 end
