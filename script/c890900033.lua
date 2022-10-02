25c6cc0fabe30696bb1ee9e3005b43c7ff2db7fc
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-02 21:04:42 +0300

    Update c890900033.lua

--- script/c890900033.lua
+++ script/c890900033.lua
@@ -39,9 +39,15 @@ s.listed_names={890900035,890900036,890900017,890900025,890900034}
 s.material_setcode=0x3dd
 s.listed_series={0x3dd}
 function s.tfilter(c,scard,sumtype,tp)
+	if c:c:IsHasEffect(890900042) then
+	return c:IsSummonCode(scard,sumtype,tp,890900035)
+	else
 	return c:IsSummonCode(scard,sumtype,tp,890900035) or c:IsHasEffect(890900032)
 end
 function s.sfilter(c,scard,sumtype,tp)
+	if c:IsHasEffect(890900042) then
+	return c:IsSummonCode(scard,sumtype,tp,890900036)
+	else
 	return c:IsSummonCode(scard,sumtype,tp,890900036) or c:IsHasEffect(890900032)
 end
 function s.atkval(e,c)
