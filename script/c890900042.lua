002042569e54db8be3f926c0fe0b2a9b2cfd0408
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-04 22:07:54 +0300

    Update c890900042.lua

--- script/c890900042.lua
+++ script/c890900042.lua
@@ -49,8 +49,8 @@ function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	end
 	local b1=g1
 	local b2=g2
-	gc=nil
-	vl=nil
+	gc=0
+	vl=0
 	if chk==0 then e:SetLabel(0) return b1 or b2 end
 	local op=aux.SelectEffect(tp,
 		{b1,aux.Stringid(id,0)},
