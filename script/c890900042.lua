444c80a468fb0bbb694f8d48dbc646a5399b41e0
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-04 21:50:41 +0300

    Update c890900042.lua

--- script/c890900042.lua
+++ script/c890900042.lua
@@ -15,9 +15,81 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_HAND+LOCATION_ONFIELD+LOCATION_GRAVE)
 	e2:SetValue(s.subval)
 	c:RegisterEffect(e2)
+	-- Effects
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,3))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,0})
+	e3:SetCost(s.descost)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0x3dd}
 function s.subval(e,c)
 	return c:IsSetCard(0x3dd)
 end
-
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(9)
+	return true
+end
+function s.filter2(c,fc)
+	if not c:IsAbleToGraveAsCost() then return false end
+	return c:IsCode(table.unpack(fc.material))
+end
+function s.filter1(c,tp)
+	return c.material and c:IsType(TYPE_FUSION) and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,c)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g1=true
+	local g2=nil
+	if e:GetLabel()==9 then
+		  g2=Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_EXTRA,0,1,nil,tp)
+	end
+	local b1=g1
+	local b2=g2
+	if chk==0 then e:SetLabel(0) return b1 or b2 end
+	local op=aux.SelectEffect(tp,
+		{b1,aux.Stringid(id,0)},
+		{b2,aux.Stringid(id,1)})
+	e:SetLabel(op)
+	if op==1 then
+		local lv=e:GetHandler():GetLevel()
+		Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	else
+		if e:GetLabel()==9 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+		local g=Duel.SelectMatchingCard(tp,s.filter1,tp,LOCATION_EXTRA,0,1,1,nil,tp)
+		Duel.ConfirmCards(1-tp,g)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local cg=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil,g: GetFirst())
+		Duel.SendtoGrave(cg,REASON_COST)
+		end
+	end
+	e:SetLabel(op,Duel.AnnounceLevel(tp,1,7,lv),cg:GetFirst():GetCode())
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local op,lv,cg=e:GetLabel()
+	local tc=Duel.GetFirstTarget()
+	if op==1 then
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+end
+	else
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e1:SetValue(cg)
+	c:RegisterEffect(e1)
+	end
+end
