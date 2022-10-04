9db80195e2f40b22fed694a228d626056f7b5da5
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-04 21:23:34 +0300

    Update c890900033.lua

--- script/c890900033.lua
+++ script/c890900033.lua
@@ -39,10 +39,10 @@ s.listed_names={890900035,890900036,890900017,890900025,890900034}
 s.material_setcode=0x3dd
 s.listed_series={0x3dd}
 function s.tfilter(c,scard,sumtype,tp)
-	return c:IsSummonCode(scard,sumtype,tp,890900035) or (c:IsHasEffect(890900042) and c:IsType(TYPE_TUNER))
+	return c:IsSummonCode(scard,sumtype,tp,890900035) or (c:IsHasEffect(890900042) and c:IsType(TYPE_TUNER)) or (c:IsHasEffect(890900032) and c:IsType(TYPE_TUNER))
 end
 function s.sfilter(c,scard,sumtype,tp)
-	return c:IsSummonCode(scard,sumtype,tp,890900036) or (c:IsHasEffect(890900042) and not c:IsType(TYPE_TUNER))
+	return c:IsSummonCode(scard,sumtype,tp,890900036) or (c:IsHasEffect(890900042) and not c:IsType(TYPE_TUNER)) or (c:IsHasEffect(890900032) and not c:IsType(TYPE_TUNER))
 end
 function s.syncheck(g,sc,tp)
 	return g:FilterCount(Card.IsHasEffect,nil,890900042)<=1
