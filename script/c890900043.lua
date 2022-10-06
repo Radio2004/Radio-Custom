9c74b941584640052a8882ba03ea521315a2cf19
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-06 21:50:54 +0300

    Update c890900043.lua

--- script/c890900043.lua
+++ script/c890900043.lua
@@ -4,6 +4,14 @@ function s.initial_effect(c)
 	--Fusion Procedure
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,s.ffilter1,s.ffilter2)
+	--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
 end
 function s.ffilter1(c,fc,sumtype,tp)
 	return c:IsAttribute(ATTRIBUTE_WIND,fc,sumtype,tp) and c:IsSetCard(0x38d,fc,sumtype,tp)
@@ -11,4 +19,6 @@ end
 function s.ffilter2(c,fc,sumtype,tp)
 	return c:IsAttribute(ATTRIBUTE_DARK,fc,sumtype,tp) and c:IsSetCard(0x3dd,fc,sumtype,tp)
 end
-
+function s.val(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_GRAVE)*200
+end
