7002eb7362bd970f8841aa11bdd57ca8cc668bcb
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-06 22:21:30 +0300

    a

---
+++ script/c890900044.lua
@@ -0,0 +1,86 @@
+--Melirria, Gaiden Story
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add to Hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Extra Material
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCode(EFFECT_EXTRA_MATERIAL)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetTargetRange(1,0)
+	e2:SetOperation(s.extracon)
+	e2:SetValue(s.extraval)
+	c:RegisterEffect(e2)
+	if s.flagmap==nil then
+		s.flagmap={}
+	end
+	if s.flagmap[c]==nil then
+		s.flagmap[c] = {}
+	end
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_HAND,0)
+	e3:SetTarget(s.eftg)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x22cd}
+-- Infinitrack monster
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x22cd) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp) -- Add to hand
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.extrafilter(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
+end
+function s.extracon(c,e,tp,sg,mg,lc,og,chk)
+	return (sg+mg):Filter(s.extrafilter,nil,e:GetHandlerPlayer()):IsExists(Card.IsSetCard,1,og,0x22cd) and
+	sg:FilterCount(s.flagcheck,nil)<2
+end
+function s.flagcheck(c)
+	return c:GetFlagEffect(id)>0
+end
+function s.extraval(chk,summon_type,e,...)
+	local c=e:GetHandler()
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_LINK or not sc:IsSetCard(0x3dd) or Duel.GetFlagEffect(tp,id)>0 then
+			return Group.CreateGroup()
+		else
+			table.insert(s.flagmap[c],c:RegisterFlagEffect(id,0,0,1))
+			return Group.FromCards(c)
+		end
+	elseif chk==1 then
+		local sg,sc,tp=...
+		if summon_type&SUMMON_TYPE_LINK == SUMMON_TYPE_LINK and #sg>0 then
+			Duel.Hint(HINT_CARD,tp,id)
+			Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+		end
+	elseif chk==2 then
+		for _,eff in ipairs(s.flagmap[c]) do
+			eff:Reset()
+		end
+		s.flagmap[c]={}
+	end
+end
+function s.eftg(e,c)
+	return c:IsSetCard(0x3dd) and c:IsCanBeLinkMaterial()
+end
