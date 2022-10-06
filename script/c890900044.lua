3734828b26d626e7f74352507f5da39d29276855
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-06 22:28:00 +0300

    Update c890900044.lua

--- script/c890900044.lua
+++ script/c890900044.lua
@@ -59,7 +59,7 @@ function s.extraval(chk,summon_type,e,...)
 		if summon_type~=SUMMON_TYPE_LINK or not sc:IsSetCard(0x3dd) or Duel.GetFlagEffect(tp,id)>0 then
 			return Group.CreateGroup()
 		else
-			table.insert(s.flagmap[c],c:RegisterFlagEffect(id,0,0,1))
+			s.flagmap[c]=c:RegisterFlagEffect(id,0,0,1)
 			return Group.FromCards(c)
 		end
 	elseif chk==1 then
@@ -69,10 +69,10 @@ function s.extraval(chk,summon_type,e,...)
 			Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
 		end
 	elseif chk==2 then
-		for _,eff in ipairs(s.flagmap[c]) do
-			eff:Reset()
+	   if s.flagmap[c] then
+			s.flagmap[c]:Reset()
+			s.flagmap[c]=nil
 		end
-		s.flagmap[c]={}
 	end
 end
 function s.eftg(e,c)
