d191c7d045d56a94b6c5c07fb73046811abfe1a3
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-07 15:47:09 +0300

    a

---
+++ script/c890900045.lua
@@ -0,0 +1,25 @@
+--Melirria, Bonds Beyond the Story
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add to Hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x22cd|0x38d|0x22c3|0x1fa3) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp) -- Add to hand
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
