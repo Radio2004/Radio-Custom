68a7193c73e2be01ae3d5fa5afcd3d2972b9417c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-07 15:58:10 +0300

    Update c890900045.lua

--- script/c890900045.lua
+++ script/c890900045.lua
@@ -9,7 +9,18 @@ function s.initial_effect(c)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--lv change
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetTarget(s.lvtg)
+	e2:SetOperation(s.lvop)
+	c:RegisterEffect(e2)
 end
+s.listed_names={id}
+s.listed_series={0x22cd+0x38d+0x22c3+0x1fa3+0x3dd}
 function s.filter(c,e,tp)
 	return (c:IsSetCard(0x22cd) or c:IsSetCard(0x38d) or c:IsSetCard(0x22c3) or c:IsSetCard(0x1fa3)) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
@@ -21,5 +32,29 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp) -- Add to hand
 		local sg=g:Select(tp,1,1,nil)
 		Duel.SendtoHand(sg,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.lvfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x3dd) and c:HasLevel()
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.lvfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.lvfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,s.lvfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LVRANK)
+	local lv=Duel.AnnounceLevel(tp,1,5,g:GetFirst():GetLevel())
+	Duel.SetTargetParam(lv)
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local lv=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
 	end
 end
