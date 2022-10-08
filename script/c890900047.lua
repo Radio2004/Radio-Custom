1c2cf7371f71643286769933642c85fb32dbec90
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-08 19:22:44 +0300

    Create c890900047.lua

---
+++ script/c890900047.lua
@@ -0,0 +1,30 @@
+--Cyberse Melirria
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--summon success
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCondition(s.sumcon)
+	e3:SetOperation(s.sumsuc)
+	c:RegisterEffect(e3)
+end
+function s.limfilter(c)
+	return c:GetSummonType()==SUMMON_TYPE_LINK and c:IsSetCard(0x3dd)
+end
+function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.limfilter,1,nil)
+end
+function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetChainLimitTillChainEnd(s.elimit)
+end
+function s.elimit(re,rp,tp)
+	return not (re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and re:IsHasType(EFFECT_TYPE_ACTIVATE))
+end
+
