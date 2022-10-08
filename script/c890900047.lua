938a1603a647b258515b4571f9bc9fed514ba3df
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-08 20:40:40 +0300

    Update c890900047.lua

--- script/c890900047.lua
+++ script/c890900047.lua
@@ -38,7 +38,7 @@ function s.lkfilter(c,oc)
 	return c:GetLinkedGroup():IsContains(oc)
 end
 function s.indtg(e,c)
-	local g=Duel.GetMatchingGroup(s.lkfilter,e:GetHandler():GetControler(),LOCATION_MZONE,0,nil,c)
+	local g=Duel.GetMatchingGroup(s.lkfilter,e:GetHandler():GetControler(),LOCATION_MZONE,0,nil,e:GetHandler())
 	return g
 end
 function s.limfilter(c)
