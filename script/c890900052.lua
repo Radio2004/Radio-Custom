edb2b483582cda6b72eb6a22c03cb962d4f6facb
Radio2004 <Andrijsupa@gmail.com>
2022-10-09 17:29:45 +0300

    Update c890900052.lua

--- script/c890900052.lua
+++ script/c890900052.lua
@@ -2,10 +2,11 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	-- 2 Rank 4 "Exorsister" Xyz Monsters
 	Xyz.AddProcedure(c,s.xyzfilter,nil,2,nil,nil,nil,nil,false)
-	-- Must be Xyz Summoned using the correct materials
 end
 function s.xyzfilter(c,xyz,sumtype,tp)
-	return (c:IsSetCard(0x3de,xyz,sumtype,tp) or c:FilterCount(Card.IsHasEffect,nil,890900042)<=1) and c:IsLevel(7)
+	if c:IsHasEffect(890900042) then
+	return c:IsSetCard(0x3de,xyz,sumtype,tp) and c:IsLevel(7)
+	else
+	return (c:IsSetCard(0x3de,xyz,sumtype,tp) or c:IsHasEffect(890900042)) and c:IsLevel(7) 
 end
