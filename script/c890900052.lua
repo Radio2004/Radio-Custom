a7c7eeee6bbb0104493ff2d99d043f71f2269579
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-09 20:18:40 +0300

    Update c890900052.lua

--- script/c890900052.lua
+++ script/c890900052.lua
@@ -2,13 +2,10 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(s.xyzfilter),7,2,nil,nil,nil,nil,false,s.xyzcheck)
+	Xyz.AddProcedure(c,nil,7,2,nil,nil,nil,nil,false,s.xyzcheck)
 end
 function s.xyzcheck(g,tp,xyz)
-	local mg=g:Filter(Card.IsHasEffect,nil,890900042)
-	return #mg<=1
-end
-
-function s.xyzfilter(c,xyz,sumtype,tp)
-	return c:IsSetCard(0x3de,xyz,sumtype,tp)
+	local ct1=g:FilterCount(Card.IsSetCard,nil,0x3de)
+	local ct2=g:FilterCount(Card.IsHasEffect,nil,890900042)
+	return ct1>0 and ct2<=1
 end
