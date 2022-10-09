bb1030b376dcf8d96d3530dffd8657124fa2f3fd
Radio2004 <Andrijsupa@gmail.com>
2022-10-09 16:59:54 +0300

    a

---
+++ script/c890900051.lua
@@ -0,0 +1,13 @@
+--Power of Melirria - Fire Phoenix
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,1,id)
+	c:EnableUnsummonable()
+	--splimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(s.splimit)
+	c:RegisterEffect(e1)
+end
