8a07944701e6ebaaf886f89ef782fe691d3abcbc
Radio2004 <Andrijsupa@gmail.com>
2022-10-09 17:42:44 +0300

    Update c890900052.lua

--- script/c890900052.lua
+++ script/c890900052.lua
@@ -5,5 +5,5 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,s.xyzfilter,nil,2,nil,nil,nil,nil,false)
 end
 function s.xyzfilter(c,xyz,sumtype,tp)
-	return (c:IsSetCard(0x3de,xyz,sumtype,tp) or c:IsHasEffect(890900042)=1) and c:IsLevel(7)
+	return (c:IsSetCard(0x3de,xyz,sumtype,tp) or c:IsHasEffect(890900042)==1) and c:IsLevel(7)
 end
