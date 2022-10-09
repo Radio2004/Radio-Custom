c466df92b62c1eea1374b56670d3b992a22ff643
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-09 20:36:38 +0300

    Update c890900052.lua

--- script/c890900052.lua
+++ script/c890900052.lua
@@ -3,6 +3,28 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(s.xyzfilter),7,2,nil,nil,nil,nil,false,s.xyzcheck)
+	--Destroy 1 Monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMING_END_PHASE+TIMING_EQUIP)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(aux.dxmcostgen(1,1,nil))
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--Name becomes "Power of Melirria - Fire Phoenix" while on the field on in GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CHANGE_CODE)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetValue(890900051)
+	c:RegisterEffect(e2)
 end
 function s.xyzcheck(g,tp,xyz)
 	local ct1=g:FilterCount(Card.IsSetCard,nil,0x3de)
@@ -12,4 +34,22 @@ end
 
 function s.xyzfilter(c,xyz,sumtype,tp)
 	return c:IsSetCard(0x3de,xyz,sumtype,tp) or c:IsHasEffect(890900042)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetFirst():GetBaseAttack())
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local dam=tc:GetBaseAttack()/
+		if dam<0 or tc:IsFacedown() then dam=0 end
+		if Duel.Destroy(tc,REASON_EFFECT)~=0 then
+			Duel.Damage(1-tp,dam,REASON_EFFECT)
+		end
+	end
 end
