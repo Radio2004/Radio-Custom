6d2184f371f5ebda4eea399a309b13543bb6b316
Radio2004 <Andrijsupa@gmail.com>
2022-10-09 18:06:17 +0300

    Update c890900052.lua

--- script/c890900052.lua
+++ script/c890900052.lua
@@ -5,5 +5,5 @@ function s.initial_effect(c)
 	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(s.xyzfilter),7,2)
 end
 function s.xyzfilter(c,xyz,sumtype,tp)
-	return c:IsSetCard(0x3de,xyz,sumtype,tp)<=1 and c:IsCode(46986415,xyz,sumtype,tp)<=1
+	return c:IsSetCard(0x3de,xyz,sumtype,tp)==1 and c:IsCode(46986415,xyz,sumtype,tp)==1
 end
