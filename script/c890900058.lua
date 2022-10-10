582bf7fd9b750b0a0d872ea6e1726c656bdb72de
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-10 16:46:36 +0300

    Update c890900058.lua

--- script/c890900058.lua
+++ script/c890900058.lua
@@ -3,13 +3,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	c:SetUniqueOnField(1,1,id)
-	--Special summon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(s.splimit)
-	c:RegisterEffect(e1)
 	--Negate
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
