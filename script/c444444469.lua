4895b51efd149f7915ef15be0146749df2eb39fe
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-10 23:58:00 +0300

    Update c444444469.lua

--- script/c444444469.lua
+++ script/c444444469.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,s.ffilter,12)
+	Fusion.AddProcMixN(c,true,true,s.ffilter,4)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -50,7 +50,7 @@ function s.initial_effect(c)
 end
 s.material_setcode=0x1bc
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
-	return c:IsSetCard(0x1bc,fc,sumtype,tp) 
+	return c:IsSetCard(0x1bc,fc,sumtype,tp) and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,sumtype,tp),fc,sumtype,tp))
 end
 function s.fusfilter(c,code,fc,sumtype,tp)
 	return c:IsSummonCode(fc,sumtype,tp,code) and not c:IsHasEffect(511002961)
