6c4fce15057e007626855e1729099d3e16d51da6
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-10 14:55:58 +0300

    a

--- script/c890900054.lua
+++ script/c890900054.lua
@@ -2,6 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
+	c:SetUniqueOnField(1,1,id)
 	--Special summon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
