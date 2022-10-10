2f532b614e82d15db4e3901561030467d1007fb2
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-10 16:09:18 +0300

    Update c890900058.lua

--- script/c890900058.lua
+++ script/c890900058.lua
@@ -3,13 +3,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	c:SetUniqueOnField(1,1,id)
-	--Special summon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(s.splimit)
-	c:RegisterEffect(e1)
 	--Negate
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
@@ -38,7 +31,7 @@ end
 s.listed_names={id-1}
 function s.splimit(e,se,sp,st)
 	local sc=se:GetHandler()
-	return sc:IsCode(id-1) or sc:IsCode(id)
+	return sc:IsCode(id-1)
 end
 function s.ngcfilter(c,tp)
 	return c:IsControler(tp) and c:IsOnField()
