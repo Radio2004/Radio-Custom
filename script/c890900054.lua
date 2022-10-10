77f8b8f2d82639b5043ef4cdce4d1114afed88af
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-10 14:52:25 +0300

    a

---
+++ script/c890900054.lua
@@ -0,0 +1,17 @@
+--Clear Power of Melirria - Red Flower Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Special summon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(s.splimit)
+	c:RegisterEffect(e1)
+end
+s.listed_names={id-1}
+function s.splimit(e,se,sp,st)
+	local sc=se:GetHandler()
+	return sc:IsCode(id-1)
+end
