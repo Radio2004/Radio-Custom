08f84b797169f1a168a2ae4b0fd23fc2a8717b5c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-10-15 23:36:51 +0300

    Update c890900002.lua

--- script/c890900002.lua
+++ script/c890900002.lua
@@ -116,7 +116,7 @@ end
 end
    function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if (c:IsAttackPos() or c:IsDefensePos()) and c:IsRelateToEffect()~=1 then
+	if (c:IsAttackPos() or c:IsDefensePos()) and c:IsRelateToEffect(e)~=1 then
 		Duel.Destroy(c,REASON_EFFECT)
 	end
 end
