7db0039a5c562ffd73e75010c9d69cd62e5f300c
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-06 18:22:06 +0200

    Update c1199900005.lua

--- script/c1199900005.lua
+++ script/c1199900005.lua
@@ -16,6 +16,18 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+	--atk down
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.atktg2)
+	e2:SetOperation(s.atkop2)
+	c:RegisterEffect(e2)
 end
 s.listed_series={0x14f1}
 s.material_setcode={0x14f1}
@@ -53,4 +65,21 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		 tc:RegisterEffect(e2)
 	end
 	Duel.SpecialSummonComplete()
+end
+function s.atkfilter(c)
+	return c:IsFaceup() and c:GetBaseAttack()~=c:GetAttack()
+end
+function s.atktg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.atkfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.atkfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.atkfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,g:GetFirst():GetAttack()//1000)
+end
+function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local mtgc=tg:GetFirst():GetAttack()//1000
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Damage(1-tp,mtgc,REASON_EFFECT)
+	end
 end
