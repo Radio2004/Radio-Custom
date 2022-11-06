53db5ab9cca51781088e2392c14083ae280bc9b9
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-06 20:11:04 +0200

    Update c1199900007.lua

--- script/c1199900007.lua
+++ script/c1199900007.lua
@@ -74,7 +74,7 @@ function s.activate2(e,tp,eg,ep,ev,re,r,rp)
 		--DEF becomes doubled of its current DEF
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetCode(EFFECT_SET_DEFENSE_FINAL)
 		e1:SetTarget(s.atkdeftarget2)
 		e1:SetTargetRange(LOCATION_MZONE,0)
 		e1:SetValue(s.defval)
