d5b710ef9bf0284da9587925738f130ad93b9bf4
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-06 19:41:43 +0200

    Update c1199900007.lua

--- script/c1199900007.lua
+++ script/c1199900007.lua
@@ -12,6 +12,7 @@ function s.initial_effect(c)
 	e2:SetCategory(CATEGORY_POSITION)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
 	e2:SetTarget(s.target)
 	e2:SetOperation(s.activate)
 	c:RegisterEffect(e2)
@@ -21,28 +22,24 @@ function s.atkfilter(c)
 	return c:IsLevelBelow(4) and c:IsFaceup()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.atkfilter,tp,LOCATION_MZONE,0,1,nil) end
+	if chk==0 then return true end
 	local sg=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil)
 	Duel.SetOperationInfo(0,CATEGORY_POSITION,sg,#sg,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local sg=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_MZONE,0,nil)
-	if #sg>0 then
-		local tc=sg:GetFirst()
-		for tc in aux.Next(sg) do
-			local e2=Effect.CreateEffect(c)
-			e2:SetType(EFFECT_TYPE_SINGLE)
-			e2:SetCode(EFFECT_SET_POSITION)
-			e2:SetValue(POS_FACEUP_ATTACK)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
-			tc:RegisterEffect(e2)
-			--Cannot change their battle positions
-			local e3=e2:Clone()
-			e3:SetDescription(3313)
-			e3:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-			e3:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
-			tc:RegisterEffect(e3)
-		end
+		--Pos Change
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_SET_POSITION)
+		e2:SetRange(LOCATION_SZONE)
+		e2:SetTargetRange(LOCATION_MZONE,0)
+		e2:SetValue(POS_FACEUP_ATTACK)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		c:RegisterEffect(e2)
+		local e3=e2:Clone()
+		e3:SetCode(EFFECT_CANNOT_CHANGE_POSITION)
+		c:RegisterEffect(e3)
 	end
 end
