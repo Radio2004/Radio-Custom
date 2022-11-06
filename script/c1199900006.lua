61dcfe995c22f71853775a1571cf80cfec6a2b79
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-06 18:50:00 +0200

    Update c1199900006.lua

--- script/c1199900006.lua
+++ script/c1199900006.lua
@@ -1,14 +1,13 @@
 --Attackers and Blockers - Dark Warrior
 local s,id=GetID()
 function s.initial_effect(c)
-	--Immune
+	--summon success
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_IMMUNE_EFFECT)
-	e1:SetCondition(s.econ)
-	e1:SetValue(s.efilter)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.regcon)
+	e1:SetOperation(s.regop)
 	c:RegisterEffect(e1)
 	--Double damage
 	local e2=Effect.CreateEffect(c)
@@ -25,6 +24,24 @@ end
 function s.efilter(e,te)
 	return not te:GetOwner():IsSetCard(0x14f1)
 end
-function s.dcon(e,tp,eg,ep,ev,re,r,rp)
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
 	return re and re:IsActiveType(TYPE_MONSTER) and re:GetHandler():IsSetCard(0x14f1)
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_IMMUNE_EFFECT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e1:SetCondition(s.econ)
+	e1:SetValue(s.efilter)
+	c:RegisterEffect(e1)
+	--Double damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+	e2:SetValue(aux.ChangeBattleDamage(1,DOUBLE_DAMAGE))
+	c:RegisterEffect(e2)
 end
