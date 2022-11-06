f45923c1494aa133bb4acc1e24a26c2d24e4acbd
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-06 19:08:41 +0200

    Update c1199900006.lua

--- script/c1199900006.lua
+++ script/c1199900006.lua
@@ -36,22 +36,6 @@ function s.regop(e,tp,eg,ep,ev,re,r,rp)
 	c:RegisterEffect(e2)
 	end
 end
-function s.regcon2(e,tp,eg,ep,ev,re,r,rp)
-	if not re then return false end
-	local rc=re:GetHandler()
-	return re:IsActiveType(TYPE_MONSTER) and rc:IsSetCard(0x14f1) and rc:IsSummonLocation(LOCATION_EXTRA)
-end
-function s.regop2(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_IMMUNE_EFFECT)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	e1:SetValue(s.efilter)
-	c:RegisterEffect(e1)
-end
 function s.efilter(e,te)
 	return not te:GetOwner():IsSetCard(0x14f1)
 end
