8f1363df897e4afaf92e5d6c0f473d4438f75995
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-06 18:11:35 +0200

    Update c1199900005.lua

--- script/c1199900005.lua
+++ script/c1199900005.lua
@@ -4,9 +4,52 @@ function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcMixN(c,false,false,aux.FilterBoolFunctionEx(Card.IsSetCard,0x14f1),1,s.ffilter,1)
+	--Special Summon 1 "0x14f1" from your Deck, hand, or GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCondition(function(e) return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) end)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
 end
+s.listed_series={0x14f1}
 s.material_setcode={0x14f1}
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
 	return c:GetBaseAttack()~=c:GetAttack()
 end
-
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x14f1) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND)
+	Duel.SetChainLimit(s.chlimit)
+end
+function s.chlimit(e,ep,tp)
+	return tp==ep
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
+	local tc=g:GetFirst()
+	if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		--Increase ATK/DEF
+		 local e1=Effect.CreateEffect(c)
+		 e1:SetType(EFFECT_TYPE_SINGLE)
+		 e1:SetCode(EFFECT_UPDATE_ATTACK)
+		 e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		 e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)		 e1:SetValue(1000)
+		 tc:RegisterEffect(e1)
+		 local e2=e1:Clone()
+		 e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		 tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
+end
