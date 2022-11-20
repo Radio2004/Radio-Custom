7fec16d62e078f488d96cb7e0e6fef4e92460e0b
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-20 17:30:38 +0200

    a

---
+++ script/c1199900012.lua
@@ -0,0 +1,22 @@
+--Max Power Attack or Defend
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	c:RegisterEffect(e1)
+	--change
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CHANGE_CODE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetValue(1199900007)
+	c:RegisterEffect(e2)
+end
+s.listed_names={1199900007}
+function s.costfilter(c)
+	return c:IsFaceup() and c:IsCode(1199900007) and c:IsAbleToGraveAsCost()
+end
