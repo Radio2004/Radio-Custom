79b1b04952a5f9af617723321462968bd3569dc9
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-20 17:52:45 +0200

    Update c1199900012.lua

--- script/c1199900012.lua
+++ script/c1199900012.lua
@@ -15,6 +15,16 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetValue(1199900007)
 	c:RegisterEffect(e2)
+	--Change opponent's attack position monsters to defense position
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(s.postcond)
+	e3:SetTarget(s.postg)
+	e3:SetOperation(s.posop)
+	c:RegisterEffect(e3)
 end
 s.listed_names={1199900007}
 function s.costfilter(c)
@@ -25,4 +35,19 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_SZONE,0,1,1,nil)
 	Duel.SendtoGrave(g,REASON_COST)
+end
+function s.postcond(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetAttackedCount()>0
+end
+function s.filter(c)
+	return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition()
+end
+function s.postg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,#g,0,0)
+end
+function s.posop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
+	Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
 end
