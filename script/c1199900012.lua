dd71e50dc74cb60aae0d74214f0ae2719cfaaa17
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2022-11-23 18:35:20 +0200

    Update c1199900012.lua

--- script/c1199900012.lua
+++ script/c1199900012.lua
@@ -37,6 +37,14 @@ function s.initial_effect(c)
 	e4:SetTarget(s.tdtg)
 	e4:SetOperation(s.tdop)
 	c:RegisterEffect(e4)
+	--cannot direct attack
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e5:SetTarget(s.atktarget)
+	c:RegisterEffect(e5)
 end
 s.listed_names={1199900007}
 function s.costfilter(c)
@@ -108,4 +116,7 @@ function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.atkval(e,c)
 	return c:GetBaseAttack()*2
+end
+function s.atktarget(e,c)
+	return c:IsFaceup() and c:GetAttack()~=c:GetBaseAttack()
 end
