68d51a9c62a416c485824b1492b4b4a2a9d02131
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-01-16 20:18:10 +0200

    a

--- script/c890900002.lua
+++ script/c890900002.lua
@@ -22,7 +22,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 	function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FilterFaceupFunction(Card.IsSetCard,0x3dd),tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x3dd),tp,LOCATION_MZONE,0,1,nil)
 end
 	function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
