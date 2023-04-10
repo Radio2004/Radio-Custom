d771e7fc54766b5bd79e285cc21cc9a3d3c3ebdd
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-04-10 22:40:15 +0300

    a

---
+++ script/c2232000001.lua
@@ -0,0 +1,33 @@
+--Night Slasher, Winteren
+local s,id=GetID()
+function s.initial_effect(c)
+	--Negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.discon)
+	e1:SetCost(s.discost)
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	if re:GetHandler():IsDisabled() or not Duel.IsChainDisablable(ev) then return false end
+	return re:IsHasCategory(CATEGORY_DISABLE)
+end
+function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+end
