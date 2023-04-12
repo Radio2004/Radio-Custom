121e4caac9d4dc24b6a1398e92c36dd12f83ce92
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-04-12 20:18:25 +0300

    Update c2232000001.lua

--- script/c2232000001.lua
+++ script/c2232000001.lua
@@ -29,7 +29,9 @@ end
 s.listed_series={0x8b8}
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
 	if re:GetHandler():IsDisabled() or not Duel.IsChainDisablable(ev) then return false end
-	ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DISABLE) or Duel.GetOperationInfo(ev,CATEGORY_NEGATE)
+	ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DISABLE)
+	ex2,tg2,tc2=Duel.GetOperationInfo(ev,CATEGORY_NEGATE)
+	if not ex,tg,tc then ex,tg,tc=ex2,tg2,tc2
 	return ex and tg~=nil and tc+tg:FilterCount(Card.IsType,nil,TYPE_MONSTER)-tg:GetCount()>0
 end
 function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
