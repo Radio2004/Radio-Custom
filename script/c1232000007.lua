9387c63070e8377c81eed154ce496a29d771440a
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-04-25 21:58:25 +0300

    Update c1232000007.lua

--- script/c1232000007.lua
+++ script/c1232000007.lua
@@ -12,9 +12,14 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.tg)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
 function s.ctfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x8b8)
