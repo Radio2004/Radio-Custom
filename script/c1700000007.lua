ae4a3e3d973fa1b8ad643a38d37fe0ce511482f2
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-04-27 22:43:30 +0300

    Update c1700000007.lua

--- script/c1700000007.lua
+++ script/c1700000007.lua
@@ -64,8 +64,9 @@ function s.valcon(e,re,r,rp)
 end
 function s.effcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	local att=e:GetLabel()
 	if c:GetOverlayGroup():IsExists(Card.IsAttribute,1,nil,e:GetLabel()) then
-	s.attr_list[tp]=s.attr_list[tp]|e:GetLabel()
+	s.attr_list[tp]=s.attr_list[tp]|att
 	for _,str in aux.GetAttributeStrings(att) do
 		e:GetHandler():RegisterFlagEffect(0,RESET_EVENT|RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,str)
 		end
