a1e1079979b002674893d03bc9e3e0461e26305f
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-01 21:20:20 +0300

    Update c1232000009.lua

--- script/c1232000009.lua
+++ script/c1232000009.lua
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--inflict damage
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_DAMAGE)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
