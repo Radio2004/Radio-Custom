2d074f8ead32289828ca61258e2926badb6762c0
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-03 20:45:23 +0300

    Update c1232000001.lua

--- script/c1232000001.lua
+++ script/c1232000001.lua
@@ -9,7 +9,7 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.condition)
+	e1:SetCondition(s.discon)
 	e1:SetCost(s.discost)
 	e1:SetTarget(s.distg)
 	e1:SetOperation(s.disop)
@@ -25,43 +25,21 @@ function s.initial_effect(c)
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	local e3=e1:Clone()
-	e3:SetCondition(s.condition2)
-	c:RegisterEffect(e3)
 end
 s.listed_series={0x8b8}
---function s.discon(e,tp,eg,ep,ev,re,r,rp)
---	if re:GetHandler():IsDisabled() or not Duel.IsChainDisablable(ev) then return false end
---	ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DISABLE)
---	ex2,tg2,tc2=Duel.GetOperationInfo(ev,CATEGORY_NEGATE)
---	return (ex and tg~=nil and tc+tg:FilterCount(Card.IsType,nil,TYPE_MONSTER)-tg:GetCount()>0) or (ex2 and tg2~=nil and tc2+tg2:FilterCount(Card.IsType,nil,TYPE_MONSTER)-tg2:GetCount()>0)
---end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsChainNegatable(ev) then return false end
-	if not re:IsActiveType(TYPE_MONSTER) and not re:IsActiveType(TYPE_TRAP) and not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
-	local tex,ttg,ttc=Duel.GetOperationInfo(ev,CATEGORY_NEGATE)
-	if not tex or ttg==nil or ttc+ttg:FilterCount(Card.IsControler,nil,tp)-#ttg<=0 then return false end
-	if re:IsActiveType(TYPE_MONSTER) then return true end
-	return re:IsActiveType(TYPE_MONSTER)
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	if re:GetHandler():IsDisabled() or not Duel.IsChainDisablable(ev) then return false end
+	ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DISABLE)
+	ex2,tg2,tc2=Duel.GetOperationInfo(ev,CATEGORY_NEGATE)
+	return (ex and tg~=nil and tc+tg:FilterCount(Card.IsType,nil,TYPE_MONSTER)-tg:GetCount()>0) or (ex2 and tg2~=nil and tc2+tg2:FilterCount(Card.IsType,nil,TYPE_MONSTER)-tg2:GetCount()>0)
 end
-function s.condition2(e,tp,eg,ep,ev,re,r,rp)
-	if not Duel.IsChainNegatable(ev) then return false end
-	if not re:IsActiveType(TYPE_MONSTER) and not re:IsActiveType(TYPE_TRAP) and not re:IsHasType(EFFECT_TYPE_ACTIVATE) then return false end
-	local tex,ttg,ttc=Duel.GetOperationInfo(ev,CATEGORY_DISABLE)
-	if not tex or ttg==nil or ttc+ttg:FilterCount(Card.IsControler,nil,tp)-#ttg<=0 then return false end
-	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev-1,CATEGORY_DAMAGE)
-	if ex and (cp~=tp or cp==PLAYER_ALL) then return true end
-	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev-1,CATEGORY_RECOVER)
-	return ex and (cp~=tp or cp==PLAYER_ALL) and Duel.IsPlayerAffectedByEffect(1-tp,EFFECT_REVERSE_RECOVER)
-end
-
 function s.discost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then return c:IsDiscardable() end
 	Duel.SendtoGrave(c,REASON_COST+REASON_DISCARD)
 end
 function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 end
