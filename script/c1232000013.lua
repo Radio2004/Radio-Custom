b0d47b85a8ef5553fddbe0ab2dc3ca4a420e6702
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-06 19:52:54 +0300

    Update c1232000013.lua

--- script/c1232000013.lua
+++ script/c1232000013.lua
@@ -71,8 +71,7 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
-	if chk==0 then return not e:GetHandler():IsStatus(STATUS_CHAINING)
-		and ft>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_HAND)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
