5f63272c6137f9cfc8b1a2417a381f40a4705cf1
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-06 19:06:36 +0300

    Create c1700000012.lua

---
+++ script/c1700000012.lua
@@ -0,0 +1,106 @@
+--Dark Virtuality Soldier - Unknown
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x7cc),6,4)
+	c:EnableReviveLimit()
+	--DARK
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetLabel(ATTRIBUTE_DARK)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetCondition(s.effcon)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--WIND
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetLabel(ATTRIBUTE_WIND)
+	e2:SetValue(s.atkval)
+	e2:SetCondition(s.effcon)
+	c:RegisterEffect(e2)
+	--Water
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e3:SetLabel(ATTRIBUTE_WATER)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.effcon)
+	e3:SetValue(s.valcon)
+	c:RegisterEffect(e3)
+	--EARTH
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_REMOVE+CATEGORY_DRAW)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetLabel(ATTRIBUTE_EARTH)
+	e4:SetCountLimit(1)
+	e4:SetCost(s.cost2)
+	e4:SetCondition(s.effcon)
+	e4:SetTarget(s.target)
+	e4:SetOperation(s.operation)
+	c:RegisterEffect(e4,false,REGISTER_FLAG_DETACH_XMAT)
+end
+function s.effcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetOverlayGroup():IsExists(Card.IsAttribute,1,nil,e:GetLabel()) and c:IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.valcon(e,re,r,rp)
+	return (r&REASON_BATTLE)~=0
+end
+function s.atkval(e,c)
+	return c:GetOverlayCount()*100
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
+end
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function s.rmfilter(c)
+	return c:IsAbleToRemove() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x7cc)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.rmfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.rmfilter,tp,LOCATION_GRAVE,0,1,nil)
+		and Duel.IsPlayerCanDraw(tp,1) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,s.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0
+		and tc:IsLocation(LOCATION_REMOVED) then
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+end
