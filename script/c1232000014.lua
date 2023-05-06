2941b4e58e356c782570e817a9129ac84706ab76
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-06 21:22:02 +0300

    Update c1232000014.lua

--- script/c1232000014.lua
+++ script/c1232000014.lua
@@ -4,6 +4,8 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--fusion material
 	Fusion.AddProcMixRep(c,false,false,aux.FilterBoolFunctionEx(Card.IsSetCard,0x8b8),3,99)
+	--Can only be 1 copy of this card on the field
+	c:SetUniqueOnField(1,1,id)
 	--destroy
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_DESTROY)
@@ -16,12 +18,24 @@ function s.initial_effect(c)
 	c:RegisterEffect(e2)
 	--set
 	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetCountLimit(1)
 	e4:SetTarget(s.settg)
 	e4:SetOperation(s.setop)
 	c:RegisterEffect(e4)
+	--draw
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,1))
+	e7:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_DECKDES)
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e7:SetProperty(EFFECT_FLAG_DELAY)
+	e7:SetCode(EVENT_TO_GRAVE)
+	e7:SetCondition(s.drcon)
+	e7:SetTarget(s.gytg)
+	e7:SetOperation(s.gyop)
+	c:RegisterEffect(e7)
 end
 s.material_setcode={0x8b8}
 s.listed_series={0x8b8}
@@ -73,5 +87,54 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 			tc:RegisterEffect(e1)
 		end
+	end
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x8b8) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.setfiltertrap(c)
+	return c:IsSetCard(0x8b8) and c:IsTrap() and c:IsSSetable()
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
+	local g=Duel.GetDecktopGroup(tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,1)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,g,#g,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_REMOVED)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.thfilter(c)
+	return c:IsSetCard(0x8b8) and c:IsAbleToHand()
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.DiscardDeck(tp,1,REASON_EFFECT)~=1 then return end
+	local tc=Duel.GetOperatedGroup():GetFirst()
+	if #tc==0 then return end
+	if tc:IsMonster() and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+	elseif tc:IsSpell() and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_REMOVED,0,1,nil) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+	elseif tc:IsTrap() and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and Duel.IsExistingMatchingCard(s.setfiltertrap,tp,LOCATION_REMOVED+LOCATION_GRAVE,0,1,nil) then
+		if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		local g=Duel.SelectMatchingCard(tp,s.setfiltertrap,tp,LOCATION_REMOVED+LOCATION_GRAVE,0,1,1,nil)
+		local tc=g:GetFirst()
+		Duel.SSet(tp,tc)
 	end
 end
