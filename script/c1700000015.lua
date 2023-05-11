7ce292f88f012800c0252a56a8bc2daf8e277dfd
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-11 21:17:03 +0300

    a

---
+++ script/c1700000015.lua
@@ -0,0 +1,51 @@
+--Virtuality Powercode Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion Materials
+	Fusion.AddProcMixRep(c,true,true,aux.FilterBoolFunctionEx(Card.IsMonster),1,99,1700000004)
+	--mill and atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_ATKCHANGE+CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetCondition(s.gycon)
+	e1:SetTarget(s.gytg)
+	e1:SetOperation(s.gyop)
+	c:RegisterEffect(e1)
+end
+function s.gycon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local ct=c:GetMaterialCount()
+	if chk==0 then return ct>0 and Duel.IsPlayerCanDiscardDeck(tp,ct) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,ct)
+	Duel.SetOperationInfo(0,CATEGORY_ATKCHANGE,c,1,tp,ct*1000)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ct=c:GetMaterialCount()
+	if Duel.DiscardDeck(tp,ct,REASON_EFFECT)>0 and c:IsRelateToEffect(e) and c:IsFaceup() then
+		local atk=Duel.GetOperatedGroup():FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)*1000
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END,2)
+		c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_ATTACK)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.ftarget)
+	e2:SetLabel(c:GetFieldID())
+	e2:SetReset(RESET_PHASE+PHASE_END,2)
+	Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.ftarget(e,c)
+	return e:GetLabel()~=c:GetFieldID()
+end
