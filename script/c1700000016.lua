d5211ce1714e38860ae3f686465da5ec1a6632c9
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-11 21:28:02 +0300

    a

---
+++ script/c1700000016.lua
@@ -0,0 +1,11 @@
+--Virtuality Fusion Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsSetCard,0x7cc))
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x7cc}
+end
