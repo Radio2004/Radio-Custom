d05212fd5f0b79160e70a5d0271c1cc33c99e604
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-12 16:38:05 +0300

    Update c1700000023.lua

--- script/c1700000023.lua
+++ script/c1700000023.lua
@@ -44,7 +44,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,nil,e,tp,e:GetHandler():GetCode()):GetFirst()
+	local tc=Duel.GetFirstMatchingCard(s.filter,tp,LOCATION_DECK,0,nil,e,tp,e:GetHandler():GetCode())
 	if tc then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
