0698bad7b910d514cf65243117305ce608267062
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-14 10:44:24 +0300

    a

--- script/c1700000018.lua
+++ script/c1700000018.lua
@@ -22,7 +22,7 @@ function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 function s.thfilter(c)
-	return c:IsSetCard(0x7cc) and c:IsType(TYPE_QUICKPLAY) and c:IsAbleToHand()
+	return c:IsType(TYPE_QUICKPLAY) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
