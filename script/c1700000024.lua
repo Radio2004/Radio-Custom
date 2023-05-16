86fb7b6ebb817a38f8f8c2a124de9752615e7c2b
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-16 23:25:39 +0300

    a

--- script/c1700000024.lua
+++ script/c1700000024.lua
@@ -31,6 +31,13 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 s.listed_series={0x7cc}
+function s.cfilter2(c,att)
+	return c:IsAttribute(att) and c:IsSetCard(0x7cc)
+end
+function s.ctfilter(c,tp)
+	local ct=c:GetLevel()
+	return c:IsAttribute(ATTRIBUTE_WIND) and c:IsSetCard(0x7cc) and Duel.IsPlayerCanDiscardDeck(tp,ct)
+end
 function s.thfilter(c,e,tp)
 	return c:IsSetCard(0x7cc) and aux.nvfilter(c) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE) and c:IsAttribute(ATTRIBUTE_DARK)
 end
@@ -42,17 +49,29 @@ function s.thfilter2(c,code)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	local remove=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	local g1=Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
 	and Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 	local g2=Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
+	local g3=Duel.IsExistingTarget(s.cfilter2,tp,LOCATION_GRAVE,0,1,nil,ATTRIBUTE_EARTH) and Duel.IsPlayerCanDraw(tp,1) and
+	local g4=Duel.IsExistingTarget(s.cfilter2,tp,LOCATION_GRAVE,0,1,nil,ATTRIBUTE_WATER) and #g>0
+	local g5=Duel.IsExistingMatchingCard(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) and Duel.IsExistingTarget(s.cfilter2,tp,LOCATION_GRAVE,0,1,nil,ATTRIBUTE_FIRE)
+	local g6=Duel.IsExistingTarget(s.ctfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
 	local b1=g1  
 	local b2=g2
-	if chk==0 then return b1 or b2 or b3 or b4 end
+	local b3=g3
+	local b4=g4
+	local b5=g5
+	local b6=g6
+	if chk==0 then return b1 or b2 or b3 or b4 or b5 end
 	local op=Duel.SelectEffect(tp,
 		{b1,aux.Stringid(id,0)},
 		{b2,aux.Stringid(id,1)},
 		{b3,aux.Stringid(id,2)},
-		{b4,aux.Stringid(id,3)})
+		{b4,aux.Stringid(id,3)},
+		{b5,aux.Stringid(id,4)},
+		{b6,aux.Stringid(id,5)})
 		e:SetLabel(op)
 	if op==1 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -60,8 +79,27 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 	elseif op==2 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-		local g=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
-		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
+		local tg=Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,tg,1,tp,0)
+	elseif op==3 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		Duel.SelectTarget(tp,s.cfilter2,tp,LOCATION_GRAVE,0,1,1,nil,ATTRIBUTE_EARTH)
+		Duel.SetTargetPlayer(tp)
+		Duel.SetTargetParam(1)
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	elseif op==4 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		Duel.SelectTarget(tp,s.cfilter2,tp,LOCATION_GRAVE,0,1,1,nil,ATTRIBUTE_WATER)
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	elseif op==5 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		Duel.SelectTarget(tp,s.cfilter2,tp,LOCATION_GRAVE,0,1,1,nil,ATTRIBUTE_FIRE)
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,remove,1,0,0)
+	elseif op==6 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+		local tg=Duel.SelectTarget(tp,s.ctfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
+		local ct=tg:GetLevel()
+		Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,ct)
 	end
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
@@ -80,5 +118,24 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			Duel.SendtoHand(g,nil,REASON_EFFECT)
 			Duel.ConfirmCards(1-tp,g)
 		end
+	elseif op==3 then
+		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+		Duel.Draw(p,d,REASON_EFFECT)
+	elseif op==4 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+		local g=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.HintSelection(g)
+			Duel.Destroy(g,REASON_EFFECT)
+		end
+	elseif op==5 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+		if #g>0 then
+			Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+		end
+	elseif op==6 then
+		local ct=tc:GetLevel()
+		Duel.DiscardDeck(tp,ct,REASON_EFFECT)
 	end
 end
