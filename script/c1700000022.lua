9ac4ecda2c5a3c5add7490a0db8f0c84afabc734
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-16 17:07:52 +0300

    Update c1700000022.lua

--- script/c1700000022.lua
+++ script/c1700000022.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_HANDES)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_BE_BATTLE_TARGET)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
@@ -20,10 +20,15 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return a:IsOnField() end
 	Duel.SetTargetCard(a)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,a,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_HANDES,0,0,1-tp,1)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:CanAttack() and not tc:IsStatus(STATUS_ATTACK_CANCELED) then
-		Duel.Destroy(tc,REASON_EFFECT)
+	if tc:IsRelateToEffect(e) and tc:CanAttack() and not tc:IsStatus(STATUS_ATTACK_CANCELED) 
+	and Duel.Destroy(tc,REASON_EFFECT)~=0 then
+		local g=Duel.GetFieldGroup(1-tp,LOCATION_HAND,0)
+		if #g==0 then return end
+		local sg=g:Select(1-tp,1,1,nil)
+		Duel.SendtoGrave(sg,REASON_DISCARD+REASON_EFFECT)
 	end
 end
