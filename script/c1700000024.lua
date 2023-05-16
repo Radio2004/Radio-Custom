d9bd6155ae9b848a7a6c50f6307c9c7f971d1472
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-16 20:47:59 +0300

    Update c1700000024.lua

--- script/c1700000024.lua
+++ script/c1700000024.lua
@@ -51,7 +51,7 @@ end
 function s.drawfilter(c,tp)
    return Duel.IsPlayerCanDraw(tp,1) and c:IsAttribute(ATTRIBUTE_EARTH)
 end
-function ts.spfilter(c,e,p)
+function s.spfilter(c,e,p)
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and aux.nvfilter(c)
 		and c:IsAttribute(ATTRIBUTE_DARK)
 			and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
