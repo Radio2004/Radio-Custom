7f65438e52d97552e4edba191929567cf76a2f61
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-16 16:58:22 +0300

    a

---
+++ script/c1700000022.lua
@@ -0,0 +1,29 @@
+--World of Virtuality Lockdown - Protocol Beta
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker():IsControler(1-tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local a=Duel.GetAttacker()
+	if chk==0 then return a:IsOnField() end
+	Duel.SetTargetCard(a)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,a,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:CanAttack() and not tc:IsStatus(STATUS_ATTACK_CANCELED) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
