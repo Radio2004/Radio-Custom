fb2f1ba81c8ca6078e41eea4c9b4bc0f2fafa81b
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-16 22:53:28 +0300

    Update c1700000024.lua

--- script/c1700000024.lua
+++ script/c1700000024.lua
@@ -47,10 +47,12 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g2=Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_GRAVE,0,1,nil,tp)
 	local b1=g1  
 	local b2=g2
-	if chk==0 then return b1 or b2 end
+	if chk==0 then return b1 or b2 or b3 or b4 end
 	local op=Duel.SelectEffect(tp,
 		{b1,aux.Stringid(id,0)},
-		{b2,aux.Stringid(id,1)})
+		{b2,aux.Stringid(id,1)},
+		{b3,aux.Stringid(id,2)},
+		{b4,aux.Stringid(id,3)})
 		e:SetLabel(op)
 	if op==1 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
@@ -62,7 +64,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,tp,0)
 	end
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local op=e:GetLabel()
 	local tc=Duel.GetFirstTarget()
