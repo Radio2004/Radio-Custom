82e32697dc88aa87e3f12f105066b2f9945c0abd
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-05-16 21:38:43 +0300

    Update c1700000024.lua

--- script/c1700000024.lua
+++ script/c1700000024.lua
@@ -108,8 +108,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local b4=s.desfilter(tc,tp)
 	local b5=s.rmfilter(tc,tp)
 	local b6=s.sfilter(tc,tp)
-	local b7= #b1 and #b2 and #b3 and #b4 and #b5 and #b6
-	if b7>1 then
+	if tc:IsAttribute()>1 then
 		Duel.Draw(tp,3,REASON_EFFECT)
 	end
 	if b1 then
