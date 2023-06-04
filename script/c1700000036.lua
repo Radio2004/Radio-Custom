b5008736bf9f6b0c8950f5af1de8e7d2b3ea472f
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-06-04 17:29:12 +0300

    Update c1700000036.lua

--- script/c1700000036.lua
+++ script/c1700000036.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Fusion materials
 	c:EnableReviveLimit()
-	Fusion.AddProcMixN(c,true,true,s.ffilter,3)
+	Fusion.AddProcMixN(c,true,true,s.ffilter,2)
 end
 function s.ffilter(c,fc,sumtype,tp)
 	return c:IsType(TYPE_MONSTER,fc,sumtype,tp) and c:IsCode(20447641,fc,sumtype,tp)
