e008ea955da00b894d66e94369ae51907410ec84
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-06-04 17:27:11 +0300

    a

---
+++ script/c1700000036.lua
@@ -0,0 +1,10 @@
+--Fusion monster test
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion materials
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,s.ffilter,3)
+end
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsType(TYPE_MONSTER,fc,sumtype,tp) and c:IsCode(20447641,,fc,sumtype,tp)
+end
