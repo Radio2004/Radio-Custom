afba042ab7ff62608303d0cc000ec44bce264107
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2023-06-04 17:40:27 +0300

    Update c1700000036.lua

--- script/c1700000036.lua
+++ script/c1700000036.lua
@@ -6,5 +6,5 @@ function s.initial_effect(c)
 	Fusion.AddProcMixN(c,true,true,s.ffilter,2)
 end
 function s.ffilter(c,fc,sumtype,tp)
-	return c:IsType(TYPE_MONSTER,fc,sumtype,tp) and c:IsCode(20447641)
+	return c:IsType(TYPE_MONSTER,fc,sumtype,tp) and c:IsCode(20447641,fc,sumtype,tp)
 end
