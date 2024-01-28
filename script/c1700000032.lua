b7a8d53c189b0723561decb795ff0da5fda741d4
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2024-01-28 18:06:14 +0200

    Update c1700000032.lua

--- script/c1700000032.lua
+++ script/c1700000032.lua
@@ -11,21 +11,12 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_XMATERIAL)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-	e3:SetValue(ATTRIBUTE_EARTH)
-	c:RegisterEffect(e3)
-	--Attribute change and prevent effect activation
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_IGNITION)
-	e4:SetCountLimit(1)
-	e4:SetCost(aux.dxmcostgen(1,1,nil))
-	e4:SetTarget(s.attg)
-	e4:SetOperation(s.atop)
-	c:RegisterEffect(e4,false,REGISTER_FLAG_DETACH_XMAT)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_OVERLAY,0)
+	e2:SetValue(ATTRIBUTE_EARTH)
 end
 
 function s.filter(c)
@@ -47,25 +38,5 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Overlay(tc,c)
 	end
 end
-
-
-function s.attg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTRIBUTE)
-	e:SetLabel(Duel.AnnounceAttribute(tp,1,ATTRIBUTE_ALL))
-end
-function s.atop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local attr=e:GetLabel()
-	--This turn, all face-up monsters on the field become that Attribute
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
-	e1:SetTargetRange(LOCATION_OVERLAY,LOCATION_OVERLAY)
-	e1:SetValue(attr)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-end
-
 
 
