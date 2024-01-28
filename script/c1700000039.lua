5989a4202c14c2b1900d8f439386ed488b68bd26
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2024-01-28 21:55:01 +0200

    Update c1700000039.lua

--- script/c1700000039.lua
+++ script/c1700000039.lua
@@ -8,12 +8,19 @@ function s.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetHintTiming(0,TIMING_DESTROY+TIMING_END_PHASE)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x7cc}
-
+function s.costfilter(c)
+	return c:IsSetCard(0x7cc) and c:IsDiscardable()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,s.costfilter,1,1,REASON_COST+REASON_DISCARD,nil)
+end
 function s.filter1(c,e,tp,tid)
 	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
 	return c:GetTurnID()==tid and (c:GetReason()&REASON_BATTLE)~=0 and #pg<=1 and c:IsSetCard(0x7cc) and (c:GetRank()>0 or c:IsStatus(STATUS_NO_LEVEL)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
