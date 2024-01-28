fb73167c295a1d075aade34138bb3d562ebb6841
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2024-01-28 20:10:42 +0200

    a

---
+++ script/c1700000035.lua
@@ -0,0 +1,45 @@
+--Virtuality Force - Lightning
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Overley
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,LOCATION_OVERLAY)
+	e2:SetTarget(s.xyzfilter)
+	e2:SetValue(ATTRIBUTE_LIGHT)
+	c:RegisterEffect(e2)
+end
+
+function s.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsImmuneToEffect(e) and c:IsRelateToEffect(e) then
+		c:CancelToGrave()
+		Duel.Overlay(tc,c)
+	end
+end
+
+function s.xyzfilter(e,c)
+	return e:GetHandler():GetOverlayGroup():IsContains(c)
+end
