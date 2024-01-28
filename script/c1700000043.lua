9e0f53aca1e04a1784502d5e74414a73ea146d0a
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2024-01-28 22:08:24 +0200

    a

---
+++ script/c1700000043.lua
@@ -0,0 +1,37 @@
+--Virtuality Greed Code
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW+CATEGORY_TODECK)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,2,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,3) and Duel.CheckRemoveOverlayCard(tp,1,0,2,REASON_EFFECT) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,3)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.CheckRemoveOverlayCard(tp,1,0,2,REASON_EFFECT) then return end
+	Duel.RemoveOverlayCard(tp,1,0,2,2,REASON_EFFECT)
+	if Duel.Draw(tp,3,REASON_EFFECT)==3 then
+		Duel.ShuffleHand(tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
+		if #g>0 then
+			Duel.BreakEffect()
+			Duel.SendtoDeck(g,nil,SEQ_DECKBOTTOM,REASON_EFFECT)
+		end
+	end
+end
