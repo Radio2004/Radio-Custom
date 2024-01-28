d22377bec31e7e0f83c8bb7ddc90fca39c8a3806
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2024-01-28 17:51:09 +0200

    Update c1700000032.lua

--- script/c1700000032.lua
+++ script/c1700000032.lua
@@ -15,8 +15,8 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_XMATERIAL)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_UPDATE_ATTACK)
-	e3:SetValue(500)
+	e3:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e3:SetValue(ATTRIBUTE_EARTH)
 	c:RegisterEffect(e3)
 end
 
