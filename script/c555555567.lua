7ca0e61eb379b405c3f3dcfc004106732169e98f
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2024-01-29 12:40:08 +0200

    Update c555555567.lua

--- script/c555555567.lua
+++ script/c555555567.lua
@@ -11,18 +11,19 @@ local e1=Effect.CreateEffect(c)
 	e1:SetTarget(s.tgtg)
 	e1:SetOperation(s.tgop)
 	c:RegisterEffect(e1)
-	end
-	function s.condition(e,tp,eg,ep,ev,re,r,rp)
+end
+
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return not Duel.IsExistingMatchingCard(aux.NOT(Card.IsSetCard),tp,LOCATION_MZONE,0,1,nil,0x22B)
 end
 function s.tgfilter(c,e,tp,lv)
 	return c:IsSetCard(0x22B) and c:IsType(TYPE_FUSION)and c:GetLevel()<lv and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function s.cfilter2(c,e,tp)
-	return c:IsType(TYPE_FUSION) and c:IsAbleToRemoveAsCost() and c:IsSetCard(0x22B)
+	return c:IsType(TYPE_FUSION) and c:IsAbleToRemoveAsCost() and c:IsSetCard(0x22B) and Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetLevel())
 end
 function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_GRAVE,0,1,nil,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local sg=Duel.SelectMatchingCard(tp,s.cfilter2,tp,LOCATION_GRAVE,0,1,1,nil,tp)
 	Duel.Remove(sg,POS_FACEUP,REASON_COST)
