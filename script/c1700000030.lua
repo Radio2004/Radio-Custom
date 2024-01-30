ce6ffd975d1e92bd3a6c13cca63ddfb13ae43465
Radio2004 <96874922+Radio2004@users.noreply.github.com>
2024-01-30 14:04:15 +0200

    Update c1700000030.lua

--- script/c1700000030.lua
+++ script/c1700000030.lua
@@ -50,7 +50,7 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetOperatedGroup()
 	if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK) then Duel.ShuffleDeck(tp)end
 	local ct=g:FilterCount(Card.IsLocation,nil,LOCATION_DECK|LOCATION_EXTRA)
-	if ct>1 then
+	if ct>1 and Duel.IsPlayerCanDraw(tp,2) then
 		Duel.BreakEffect()
 		Duel.Draw(tp,2,REASON_EFFECT)
 	end
